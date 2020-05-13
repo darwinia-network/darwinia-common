51eaa2cb74b8f5814e533286546a826f0ce8605c
Xavier Lau <c.estlavie@icloud.com>
2020-05-13 16:39:28 +0800

    update: balances RPC

--- frame/balances/rpc/runtime-api/src/lib.rs
+++ frame/balances/rpc/runtime-api/src/lib.rs
@@ -8,44 +8,14 @@
 
 // --- crates ---
 use codec::{Codec, Decode, Encode};
-#[cfg(feature = "std")]
-use serde::{Deserialize, Deserializer, Serialize, Serializer};
 // --- substrate ---
 use sp_api::decl_runtime_apis;
 use sp_runtime::traits::{MaybeDisplay, MaybeFromStr};
 
-#[derive(Default, Eq, PartialEq, Encode, Decode)]
-#[cfg_attr(feature = "std", derive(Debug, Serialize, Deserialize))]
-#[cfg_attr(feature = "std", serde(rename_all = "camelCase"))]
-pub struct RuntimeDispatchInfo<Balance> {
-	#[cfg_attr(
-		feature = "std",
-		serde(bound(serialize = "Balance: std::fmt::Display"))
-	)]
-	#[cfg_attr(feature = "std", serde(serialize_with = "serialize_as_string"))]
-	#[cfg_attr(
-		feature = "std",
-		serde(bound(deserialize = "Balance: std::str::FromStr"))
-	)]
-	#[cfg_attr(feature = "std", serde(deserialize_with = "deserialize_from_string"))]
-	pub usable_balance: Balance,
-}
-
-#[cfg(feature = "std")]
-fn serialize_as_string<S: Serializer, T: std::fmt::Display>(
-	t: &T,
-	serializer: S,
-) -> Result<S::Ok, S::Error> {
-	serializer.serialize_str(&t.to_string())
-}
-
-#[cfg(feature = "std")]
-fn deserialize_from_string<'de, D: Deserializer<'de>, T: std::str::FromStr>(
-	deserializer: D,
-) -> Result<T, D::Error> {
-	let s = String::deserialize(deserializer)?;
-	s.parse::<T>()
-		.map_err(|_| serde::de::Error::custom("Parse from string failed"))
+darwinia_support::impl_runtime_dispatch_info! {
+	struct RuntimeDispatchInfo<Balance> {
+		usable_balance: Balance
+	}
 }
 
 decl_runtime_apis! {
