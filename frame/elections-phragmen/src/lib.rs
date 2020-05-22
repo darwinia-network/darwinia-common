b1e51bd710725f77c59a35cb84f5a42d4891dd72
Xavier Lau <c.estlavie@icloud.com>
2020-05-22 14:33:43 +0800

    fix: merge issue

--- frame/elections-phragmen/src/lib.rs
+++ frame/elections-phragmen/src/lib.rs
@@ -175,7 +175,7 @@ pub trait Trait: frame_system::Trait {
 }
 
 decl_storage! {
-	trait Store for Module<T: Trait> as PhragmenElection {
+	trait Store for Module<T: Trait> as DarwiniaPhragmenElection {
 		// ---- State
 		/// The current elected membership. Sorted based on account id.
 		pub Members get(fn members): Vec<(T::AccountId, BalanceOf<T>)>;
