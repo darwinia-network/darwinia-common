faac58f65a3b396f6fec3c6bc5da2309e2874dd4
Xavier Lau <c.estlavie@icloud.com>
2020-06-08 15:33:36 +0800

    update: storage name
    might need a migration here

--- frame/bridge/eth/linear-relay/src/lib.rs
+++ frame/bridge/eth/linear-relay/src/lib.rs
@@ -201,7 +201,7 @@ pub struct EthReceiptProof {
 }
 
 decl_storage! {
-	trait Store for Module<T: Trait> as DarwiniaEthRelay {
+	trait Store for Module<T: Trait> as DarwiniaEthLinearRelay {
 		/// Anchor block that works as genesis block
 		pub GenesisHeader get(fn begin_header): Option<EthHeader>;
 
