7164e014d1ff3ecfe4390ff450467768cde689d4
Xavier Lau <c.estlavie@icloud.com>
2020-07-01 16:50:58 +0800

    add: `jump_round_should_fail`

--- frame/bridge/relayer-game/src/tests.rs
+++ frame/bridge/relayer-game/src/tests.rs
@@ -16,15 +16,36 @@ fn empty_proposal_should_fail() {
 #[test]
 fn duplicate_game_should_fail() {
 	ExtBuilder::default().build().execute_with(|| {
-		let chain = vec![MockTcHeader::new_raw(1, true)];
+		let proposal_chain = vec![MockTcHeader::new_raw(1, true)];
 
 		assert_ok!(RelayerGame::submit_proposal(
 			Origin::signed(1),
-			chain.clone()
+			proposal_chain.clone()
 		));
 		assert_err!(
-			RelayerGame::submit_proposal(Origin::signed(1), chain),
+			RelayerGame::submit_proposal(Origin::signed(1), proposal_chain),
 			RelayerGameError::ProposalAE
 		);
+	});
+}
+
+#[test]
+fn jump_round_should_fail() {
+	ExtBuilder::default().build().execute_with(|| {
+		let mut proposal_chain = vec![MockTcHeader::new_raw(1, true)];
+
+		assert_ok!(RelayerGame::submit_proposal(
+			Origin::signed(1),
+			proposal_chain.clone()
+		));
+
+		for i in 2..5 {
+			proposal_chain.push(MockTcHeader::new_raw(i, true));
+
+			assert_err!(
+				RelayerGame::submit_proposal(Origin::signed(1), proposal_chain.clone()),
+				RelayerGameError::RoundMis
+			);
+		}
 	});
 }
