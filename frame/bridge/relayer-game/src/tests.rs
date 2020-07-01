65d7f04e970dd0cb46613a0f5b757d67ad8158aa
Xavier Lau <c.estlavie@icloud.com>
2020-07-01 15:59:47 +0800

    add: `duplicate_game_should_fail`

--- frame/bridge/relayer-game/src/tests.rs
+++ frame/bridge/relayer-game/src/tests.rs
@@ -1,7 +1,7 @@
 // --- substrate ---
 use frame_support::{assert_err, assert_ok};
 // --- darwinia ---
-use crate::mock::*;
+use crate::mock::{mock_relay::MockTcHeader, *};
 
 #[test]
 fn empty_proposal_should_fail() {
@@ -9,6 +9,22 @@ fn empty_proposal_should_fail() {
 		assert_err!(
 			RelayerGame::submit_proposal(Origin::signed(1), vec![]),
 			RelayerGameError::ProposalI
+		);
+	});
+}
+
+#[test]
+fn duplicate_game_should_fail() {
+	ExtBuilder::default().build().execute_with(|| {
+		let chain = vec![MockTcHeader::new_raw(1, true)];
+
+		assert_ok!(RelayerGame::submit_proposal(
+			Origin::signed(1),
+			chain.clone()
+		));
+		assert_err!(
+			RelayerGame::submit_proposal(Origin::signed(1), chain),
+			RelayerGameError::ProposalAE
 		);
 	});
 }
