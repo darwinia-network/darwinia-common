a86d144f914dff665372453d412151d9ef58c2a8
clearloop <udtrokia@gmail.com>
2020-07-21 23:55:01 +0800

    Using the codec of HeaderThing structs in the test data of ETH Relay (#196)
    * perf(eth-relay): using the codec of eth_header_thing structs in test data
    
    * perf(eth-relay): optimize the code style of eth relay tests
    
    * perf(eth-relay): remove dup files in test data
    
    * perf(eth-relay): remove unreachable comments

--- frame/bridge/ethereum/relay/src/tests.rs
+++ frame/bridge/ethereum/relay/src/tests.rs
@@ -11,78 +11,50 @@ use frame_support::assert_ok;
 #[test]
 fn test_check_test_date_decoding() {
 	ExtBuilder::default().build().execute_with(|| {
-		let header_thing = from_file_to_eth_header_thing("./src/test-data/0.json");
-		assert_eq!(header_thing.eth_header.number, 0);
-		let header_thing = from_file_to_eth_header_thing("./src/test-data/1.json");
-		assert_eq!(header_thing.eth_header.number, 1);
-		assert_eq!(header_thing.ethash_proof.len(), 64);
+		let header_things = header_things().unwrap();
+		let suit = [
+			(&header_things[0], 0_u64, 64),
+			(&header_things[1], 1_u64, 64),
+			(&header_things[2], 2_u64, 64),
+			(&header_things[3], 3_u64, 64),
+		];
 
-		let header_thing = from_file_to_eth_header_thing("./src/test-data/2.json");
-		assert_eq!(header_thing.eth_header.number, 2);
-		assert_eq!(header_thing.ethash_proof.len(), 64);
-
-		let header_thing = from_file_to_eth_header_thing("./src/test-data/3.json");
-		assert_eq!(header_thing.eth_header.number, 3);
-		assert_eq!(header_thing.ethash_proof.len(), 64);
-
-		let header_thing = from_file_to_eth_header_thing("./src/test-data/8996776.json");
-		assert_eq!(header_thing.eth_header.number, 8996776);
-		assert_eq!(header_thing.ethash_proof.len(), 64);
-		assert_eq!(
-			EthRelay::verify_block_seal(&header_thing.eth_header, &header_thing.ethash_proof),
-			true
-		);
+		suit.iter().for_each(|(ht, n, pl)| {
+			assert_eq!(&ht.eth_header.number, n);
+			assert_eq!(&ht.ethash_proof.len(), pl);
+		});
 	})
 }
 
 #[test]
 fn test_verify_test_data_mmr_proof() {
 	ExtBuilder::default().build().execute_with(|| {
-		let header_thing_1 = from_file_to_eth_header_thing("./src/test-data/1.json");
-		let header_thing_2 = from_file_to_eth_header_thing("./src/test-data/2.json");
-		let header_thing_3 = from_file_to_eth_header_thing("./src/test-data/3.json");
-		assert_eq!(
-			EthRelay::verify_mmr(
-				header_thing_3.eth_header.number,
-				array_unchecked!(header_thing_3.mmr_root, 0, 32).into(),
-				header_thing_2
-					.mmr_proof
-					.iter()
-					.map(|h| array_unchecked!(h, 0, 32).into())
-					.collect(),
-				vec![(
-					header_thing_2.eth_header.number,
-					array_unchecked!(header_thing_2.eth_header.hash.unwrap(), 0, 32).into(),
-				)]
-			),
-			true
-		);
-		assert_eq!(
-			EthRelay::verify_mmr(
-				header_thing_3.eth_header.number,
-				array_unchecked!(header_thing_3.mmr_root, 0, 32).into(),
-				header_thing_1
-					.mmr_proof
-					.iter()
-					.map(|h| array_unchecked!(h, 0, 32).into())
-					.collect(),
-				vec![(
-					header_thing_1.eth_header.number,
-					array_unchecked!(header_thing_1.eth_header.hash.unwrap(), 0, 32).into(),
-				)]
-			),
-			true
-		);
+		let header_things = header_things().unwrap();
+		&header_things.iter().for_each(|ht| {
+			assert_eq!(
+				EthRelay::verify_mmr(
+					header_things[3].eth_header.number,
+					array_unchecked!(header_things[3].mmr_root, 0, 32).into(),
+					ht.mmr_proof
+						.iter()
+						.map(|h| array_unchecked!(h, 0, 32).into())
+						.collect(),
+					vec![(
+						ht.eth_header.number,
+						array_unchecked!(ht.eth_header.hash.unwrap(), 0, 32).into(),
+					)]
+				),
+				true
+			);
+		});
 	})
 }
 
 #[test]
 fn test_store_header() {
 	ExtBuilder::default().build().execute_with(|| {
-		let header_thing = from_file_to_eth_header_thing("./src/test-data/1.json");
-
+		let header_thing = &header_things().unwrap()[1];
 		assert_eq!(header_thing.eth_header.number, 1);
-
 		assert_ok!(<EthRelay as Relayable>::store_header(
 			Box::new(header_thing).encode()
 		));
@@ -93,8 +65,7 @@ fn test_store_header() {
 #[test]
 fn test_verify_raw_header_thing() {
 	ExtBuilder::default().build().execute_with(|| {
-		let header_thing = from_file_to_eth_header_thing("./src/test-data/2.json");
-
+		let header_thing = &header_things().unwrap()[2];
 		assert_ok!(<EthRelay as Relayable>::verify_raw_header_thing(
 			Box::new(header_thing).encode(),
 			false
@@ -105,22 +76,18 @@ fn test_verify_raw_header_thing() {
 #[test]
 fn test_verify_raw_header_thing_chain() {
 	ExtBuilder::default().build().execute_with(|| {
-		let header_thing = from_file_to_eth_header_thing("./src/test-data/0.json");
+		let mut header_things = header_things().unwrap();
+		header_things.reverse();
 
 		assert_ok!(<EthRelay as Relayable>::store_header(
-			Box::new(header_thing).encode()
+			Box::new(&header_things[3]).encode()
 		));
-		let test_data_files = vec![
-			"./src/test-data/3.json",
-			"./src/test-data/2.json",
-			"./src/test-data/1.json",
-		];
-		let raw_header_thing_chain: Vec<RawHeaderThing> = test_data_files
-			.iter()
-			.map(|f| Box::new(from_file_to_eth_header_thing(f)).encode())
-			.collect();
+
 		assert_ok!(<EthRelay as Relayable>::verify_raw_header_thing_chain(
-			raw_header_thing_chain
+			header_things[..3]
+				.iter()
+				.map(|ht| Box::new(ht).encode())
+				.collect::<Vec<RawHeaderThing>>()
 		));
 	})
 }
