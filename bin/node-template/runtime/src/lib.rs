5abed5a8a21ce9045bd8a8dcaf3165f84c9d49f9
Xavier Lau <c.estlavie@icloud.com>
2020-07-24 11:13:39 +0800

    update: substrate 6334

--- bin/node-template/runtime/src/lib.rs
+++ bin/node-template/runtime/src/lib.rs
@@ -57,9 +57,8 @@ pub mod impls {
 	}
 
 	// --- substrate ---
-	use frame_support::traits::{Currency, Get, Imbalance, OnUnbalanced};
-	use pallet_transaction_payment::Multiplier;
-	use sp_runtime::{traits::Convert, FixedPointNumber, Perquintill};
+	use frame_support::traits::{Currency, Imbalance, OnUnbalanced};
+	use sp_runtime::traits::Convert;
 	// --- darwinia ---
 	use crate::{primitives::*, *};
 
@@ -98,58 +97,6 @@ pub mod impls {
 	impl Convert<u128, Balance> for CurrencyToVoteHandler {
 		fn convert(x: u128) -> Balance {
 			x * Self::factor()
-		}
-	}
-
-	/// Update the given multiplier based on the following formula
-	///
-	///   diff = (previous_block_weight - target_weight)/max_weight
-	///   v = 0.00004
-	///   next_weight = weight * (1 + (v * diff) + (v * diff)^2 / 2)
-	///
-	/// Where `target_weight` must be given as the `Get` implementation of the `T` generic type.
-	/// https://research.web3.foundation/en/latest/polkadot/Token%20Economics/#relay-chain-transaction-fees
-	pub struct TargetedFeeAdjustment<T>(sp_std::marker::PhantomData<T>);
-
-	impl<T: Get<Perquintill>> Convert<Multiplier, Multiplier> for TargetedFeeAdjustment<T> {
-		fn convert(multiplier: Multiplier) -> Multiplier {
-			let max_weight = MaximumBlockWeight::get();
-			let block_weight = System::block_weight().total().min(max_weight);
-			let target_weight = (T::get() * max_weight) as u128;
-			let block_weight = block_weight as u128;
-
-			// determines if the first_term is positive
-			let positive = block_weight >= target_weight;
-			let diff_abs = block_weight.max(target_weight) - block_weight.min(target_weight);
-			// safe, diff_abs cannot exceed u64.
-			let diff = Multiplier::saturating_from_rational(diff_abs, max_weight.max(1));
-			let diff_squared = diff.saturating_mul(diff);
-
-			// 0.00004 = 4/100_000 = 40_000/10^9
-			let v = Multiplier::saturating_from_rational(4, 100_000);
-			// 0.00004^2 = 16/10^10 Taking the future /2 into account... 8/10^10
-			let v_squared_2 = Multiplier::saturating_from_rational(8, 10_000_000_000u64);
-
-			let first_term = v.saturating_mul(diff);
-			let second_term = v_squared_2.saturating_mul(diff_squared);
-
-			if positive {
-				// Note: this is merely bounded by how big the multiplier and the inner value can go,
-				// not by any economical reasoning.
-				let excess = first_term.saturating_add(second_term);
-				multiplier.saturating_add(excess)
-			} else {
-				// Defensive-only: first_term > second_term. Safe subtraction.
-				let negative = first_term.saturating_sub(second_term);
-				multiplier
-					.saturating_sub(negative)
-					// despite the fact that apply_to saturates weight (final fee cannot go below 0)
-					// it is crucially important to stop here and don't further reduce the weight fee
-					// multiplier. While at -1, it means that the network is so un-congested that all
-					// transactions have no weight fee. We stop here and only increase if the network
-					// became more busy.
-					.max(Multiplier::saturating_from_integer(-1))
-			}
 		}
 	}
 
@@ -340,6 +287,7 @@ use pallet_grandpa::{
 };
 use pallet_im_online::sr25519::AuthorityId as ImOnlineId;
 use pallet_session::historical as pallet_session_historical;
+use pallet_transaction_payment::{Multiplier, TargetedFeeAdjustment};
 use pallet_transaction_payment_rpc_runtime_api::RuntimeDispatchInfo as TransactionPaymentRuntimeDispatchInfo;
 use sp_api::impl_runtime_apis;
 use sp_authority_discovery::AuthorityId as AuthorityDiscoveryId;
@@ -355,7 +303,7 @@ use sp_runtime::{
 		Saturating,
 	},
 	transaction_validity::{TransactionPriority, TransactionSource, TransactionValidity},
-	ApplyExtrinsicResult, ModuleId, PerThing, Perbill, Percent, Permill, Perquintill, RuntimeDebug,
+	ApplyExtrinsicResult, ModuleId, Perbill, Percent, Permill, Perquintill, RuntimeDebug,
 };
 use sp_staking::SessionIndex;
 use sp_std::prelude::*;
@@ -485,20 +433,16 @@ impl pallet_timestamp::Trait for Runtime {
 parameter_types! {
 	pub const TransactionByteFee: Balance = 10 * MICRO;
 	pub const TargetBlockFullness: Perquintill = Perquintill::from_percent(25);
+	pub AdjustmentVariable: Multiplier = Multiplier::saturating_from_rational(1, 100_000);
+	pub MinimumMultiplier: Multiplier = Multiplier::saturating_from_rational(1, 1_000_000_000u128);
 }
-// for a sane configuration, this should always be less than `AvailableBlockRatio`.
-const_assert!(
-	TargetBlockFullness::get().deconstruct()
-		< (AvailableBlockRatio::get().deconstruct() as <Perquintill as PerThing>::Inner)
-			* (<Perquintill as PerThing>::ACCURACY
-				/ <Perbill as PerThing>::ACCURACY as <Perquintill as PerThing>::Inner)
-);
 impl pallet_transaction_payment::Trait for Runtime {
 	type Currency = Ring;
 	type OnTransactionPayment = DealWithFees;
 	type TransactionByteFee = TransactionByteFee;
 	type WeightToFee = IdentityFee<Balance>;
-	type FeeMultiplierUpdate = TargetedFeeAdjustment<TargetBlockFullness>;
+	type FeeMultiplierUpdate =
+		TargetedFeeAdjustment<Self, TargetBlockFullness, AdjustmentVariable, MinimumMultiplier>;
 }
 
 parameter_types! {
