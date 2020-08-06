7bfae988c48467b447ddf125e13180284c3db897
Xavier Lau <c.estlavie@icloud.com>
2020-08-06 14:01:08 +0800

    update: ci

---
+++ .maintain/ci/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -eux
+
+rustup default $RUST_TOOLCHAIN
+
+source ~/.cargo/env
+
+rustup --version
+cargo --version
+rustc --version
+
+cargo build --all --locked "$@"
+echo -e "\e[0;32m +-------------+ \n\e[0;32m | Build Pass | \n\e[0;32m +-------------+ \e[0m"
