1fa7ae4af858b1b74222f2b25d70e538a625c51b
Aki <wuminzhe@gmail.com>
2020-09-07 18:35:03 +0800

    Add payout_fraction to test spec

--- frame/staking/src/inflation_tests.rs
+++ frame/staking/src/inflation_tests.rs
@@ -7,15 +7,15 @@ fn compute_total_payout_should_work_correctly() {
 	let initial_issuance = 200_000;
 	let hard_cap = 1_000_000;
 
-	// year, inflated tokens, inflation rate
+	// year, inflated tokens, inflation rate, payout_fraction
 	let inflation_spec = [
-		(1, 8000 , 4.00),
-		(2, 11177, 5.37),
-		(3, 13474, 6.15),
-		(4, 15270, 6.56),
-		(5, 16713, 6.74),
-		(6, 17882, 6.76),
-		(7, 18826, 6.66),
+		(1, 8000 , 4.00, 0),
+		(2, 11177, 5.37, 35),
+		(3, 13474, 6.15, 50),
+		(4, 15270, 6.56, 77),
+		(5, 16713, 6.74, 33),
+		(6, 17882, 6.76, 81),
+		(7, 18826, 6.66, 100),
 	];
 
 	let mut total_left: RingBalance<Test> = hard_cap - initial_issuance;
@@ -23,25 +23,20 @@ fn compute_total_payout_should_work_correctly() {
 		let year = spec_item.0;
 		let expected_inflated = spec_item.1;
 		let expected_inflation_rate = spec_item.2;
-		// println!("{} - {}", year, total_left);
+		let expected_payout_fraction = spec_item.3;
+		println!("{} - {}", year, total_left);
 
 		let era_duration = MILLISECONDS_PER_YEAR;
 		let living_time = (year - 1) * MILLISECONDS_PER_YEAR;
-		let payout_fraction = Perbill::from_percent(0);
+		let payout_fraction = Perbill::from_percent(expected_payout_fraction);
 
 		let (payout, inflated) = inflation::compute_total_payout::<Test>(era_duration, living_time, total_left, payout_fraction);
 
-		assert_eq!(payout, 0);
+		assert_eq!(payout * 100, expected_inflated * expected_payout_fraction as u128);
 		assert_eq!(inflated, expected_inflated);
-		assert_eq!(inflated*100/total_left, (expected_inflation_rate*100.0) as u128);
+		println!("{}", inflated);
+		assert_eq!(inflated * 100 / (hard_cap - total_left), expected_inflation_rate as u128);
 
 		total_left = total_left - inflated;
 	}
 }
-
-// #[test]
-// fn compute_total_payout_with_payout_fraction_should_work_corrently() {
-
-// }
-
-
