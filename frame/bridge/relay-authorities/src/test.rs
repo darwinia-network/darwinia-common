2c0efe93442327c95ce48e8e287a2e37572a1076
Xavier Lau <c.estlavie@icloud.com>
2020-12-07 16:25:10 +0800

    test: insufficient stake should fail

--- frame/bridge/relay-authorities/src/test.rs
+++ frame/bridge/relay-authorities/src/test.rs
@@ -10,6 +10,10 @@ fn request_authority(account_id: AccountId) -> DispatchResult {
 	RelayAuthorities::request_authority(Origin::signed(account_id), 1, [0; 20])
 }
 
+fn request_authority_with_stake(account_id: AccountId, stake: Balance) -> DispatchResult {
+	RelayAuthorities::request_authority(Origin::signed(account_id), stake, [0; 20])
+}
+
 #[test]
 fn duplicate_request_should_fail() {
 	new_test_ext().execute_with(|| {
@@ -21,5 +25,38 @@ fn duplicate_request_should_fail() {
 
 		// Already is authority
 		assert_err!(request_authority(1), RelayAuthoritiesError::AuthorityAE);
+	});
+}
+
+#[test]
+fn insufficient_stake_should_fail() {
+	new_test_ext().execute_with(|| {
+		assert_err!(request_authority(0), RelayAuthoritiesError::StakeIns);
+
+		let max_candidates = <MaxCandidates as Get<usize>>::get() as _;
+
+		for i in 1..=max_candidates {
+			assert_ok!(request_authority_with_stake(i, i as Balance * 10));
+		}
+
+		// The minimum stake around candidates is 10 and the queue is full
+		let _ = Ring::deposit_creating(&123, 1);
+		assert_err!(request_authority(123), RelayAuthoritiesError::StakeIns);
+
+		for i in 1..=max_candidates {
+			assert!(RelayAuthorities::candidates()
+				.iter()
+				.position(|candidate| candidate == &i)
+				.is_some());
+		}
+
+		// Increase the stake to run for the candidates seat
+		let _ = Ring::deposit_creating(&123, 11);
+		assert_ok!(request_authority_with_stake(123, 11));
+
+		assert!(RelayAuthorities::candidates()
+			.iter()
+			.position(|candidate| candidate == &1)
+			.is_none());
 	});
 }
