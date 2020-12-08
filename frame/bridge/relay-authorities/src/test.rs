ae554d7ad87020d0b34ee5d8f7f024a7c6d16b7b
Xavier Lau <c.estlavie@icloud.com>
2020-12-08 14:46:57 +0800

    fix: merge

--- frame/bridge/relay-authorities/src/test.rs
+++ frame/bridge/relay-authorities/src/test.rs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // --- substrate ---
 use frame_support::{assert_err, assert_ok};
 // --- darwinia ---
@@ -159,6 +158,3 @@ fn kill_candidates_should_work() {
 		assert!(RelayAuthorities::candidates().is_empty());
 	});
 }
-=======
-
->>>>>>> origin/master
