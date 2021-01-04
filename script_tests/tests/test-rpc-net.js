42e063f3171c3f3fbe7e4e801e82bc48167e7053
echo <hujw77@gmail.com>
2021-01-04 15:50:08 +0800

    dvm: finish rpc test

--- script_tests/tests/test-rpc-net.js
+++ script_tests/tests/test-rpc-net.js
@@ -1,22 +1,19 @@
-const expect = require('chai').expect;
-const Web3 = require('web3');
-const utils = require('./utils');
-const conf = require('./config.js');
+const expect = require("chai").expect;
+const Web3 = require("web3");
+const utils = require("./utils");
+const conf = require("./config.js");
 const web3 = new Web3(conf.host);
 
+describe("Test Net API", function () {
+	it("should get current network ID", async function () {
+		expect(await web3.eth.net.getId()).to.be.equal(43);
+	});
 
-describe('Test Net API', function () {
+	it("should check if the node is listening for peer", async function () {
+		expect(await web3.eth.net.isListening()).to.be.equal(true);
+	});
 
-    it("should get current network ID", async function () {
-        expect(await web3.eth.net.getId()).to.be.equal(43);
-    });
-
-    it("should check if the node is listening for peer", async function () {
-        expect(await web3.eth.net.isListening()).to.be.equal(true);
-    });
-
-    it("should get the number of peers connected to", async function () {
-        expect(await web3.eth.net.getPeerCount()).to.be.equal(0);
-    });
+	it.skip("should get the number of peers connected to", async function () {
+		expect(await web3.eth.net.getPeerCount()).to.be.equal(0);
+	});
 });
-
--- script_tests/tests/utils.js
+++ script_tests/tests/utils.js
@@ -1,31 +1,30 @@
-const assert = require('chai').assert;
-const Web3 = require('web3');
-const BigNumber = require('bignumber.js')
-const conf = require('./config.js');
+const assert = require("chai").assert;
+const Web3 = require("web3");
+const BigNumber = require("bignumber.js");
+const conf = require("./config.js");
 const web3 = new Web3(conf.host);
 
 function customRequest(method, params) {
-    return new Promise((resolve, reject) => {
-        web3.currentProvider.send({
-            method: method,
-            params: params,
-            jsonrpc: "2.0",
-            id: conf.rpcMessageId++
-        }, function (error, result) {
-            if (error) {
-                reject(
-                    `Failed to send custom request (${method} (${params.join(",")})): ${
-                        error.message || error.toString()
-                    }`
-                );
-            }
-            resolve(result)
-        })
-    })
+	return new Promise((resolve, reject) => {
+		web3.currentProvider.send(
+			{
+				method: method,
+				params: params,
+				jsonrpc: "2.0",
+				id: conf.rpcMessageId++,
+			},
+			function (error, result) {
+				if (error) {
+					reject(
+						`Failed to send custom request (${method} (${params.join(",")})): ${
+							error.message || error.toString()
+						}`
+					);
+				}
+				resolve(result);
+			}
+		);
+	});
 }
 
-function fromDecimal(number){
-    return '0x' + new BigNumber((number).toString(10),10).toString(16);
-}
-
-module.exports = {customRequest, fromDecimal};
+module.exports = { customRequest };
