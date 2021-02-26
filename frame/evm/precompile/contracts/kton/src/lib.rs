5f15c463d73acac92764b1ea338a8dba85209ce6
ouwenkg <2630582710@qq.com>
2021-02-26 13:35:55 +0800

    Refactor

--- frame/evm/precompile/contracts/kton/src/lib.rs
+++ frame/evm/precompile/contracts/kton/src/lib.rs
@@ -18,6 +18,9 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
+mod util;
+
+use codec::Decode;
 use frame_support::traits::{Currency, ExistenceRequirement};
 use sha3::Digest;
 use sp_core::{H160, U256};
@@ -33,7 +36,10 @@ use ethabi::{Function, Param, ParamType, Token};
 use evm::{Context, ExitError, ExitSucceed};
 use sp_std::borrow::ToOwned;
 
-const DEPOSIT_FUNC: &[u8] = b"deposit(address,uint256)";
+type AccountId<T> = <T as frame_system::Trait>::AccountId;
+
+const TRANSFER_AND_CALL_ACTION: &[u8] = b"transfer_and_call(address,address,uint256)";
+const WITHDRAW_ACTION: &[u8] = b"withdraw(bytes32,uint256)";
 const KTON_PRECOMPILE: &str = "0000000000000000000000000000000000000016";
 /// Kton Precompile Contract
 ///
@@ -43,11 +49,7 @@ pub struct Kton<T: Trait> {
 }
 
 impl<T: Trait> Precompile for Kton<T> {
-	// The execute process of Kton precompile, the input data consists of four parts:
-	// 1. Wkton solidity contract deployed address(20 bytes)
-	// 2. The function selector(4 bytes)
-	// 3. The deposit kton evm address(20 bytes)
-	// 4. The deposit kton value(32 bytes)
+	// The execute process of Kton precompile
 	fn execute(
 		input: &[u8],
 		target_limit: Option<usize>,
@@ -56,59 +58,136 @@ impl<T: Trait> Precompile for Kton<T> {
 		let helper = U256::from(10)
 			.checked_pow(U256::from(9))
 			.unwrap_or(U256::MAX);
-		let deposit_func = &sha3::Keccak256::digest(&DEPOSIT_FUNC)[0..4];
-		// decode input
-		let input = InputData::decode(input)?;
-
+		let action = which_action::<T>(&input)?;
 		let con_caller = T::AddressMapping::into_account_id(context.caller);
-		if hex::encode(input.func_selector) == hex::encode(&deposit_func) {
-			// 1. Transfer kton from sender to kton erc20 contract
-			let wkton_account_id = T::AddressMapping::into_account_id(input.wkton_address);
-			let transfer_value = input.value.saturating_mul(helper);
-			T::KtonCurrency::transfer(
-				&con_caller,
-				&wkton_account_id,
-				transfer_value.low_u128().unique_saturated_into(),
-				ExistenceRequirement::AllowDeath,
-			)
-			.map_err(|_| ExitError::Other("Transfer in Kton precompile failed".into()))?;
+		match action {
+			Action::TransferAndCall(tacd) => {
+				// 1. Transfer kton from sender to kton erc20 contract
+				let wkton_account_id = T::AddressMapping::into_account_id(tacd.wkton_address);
+				let transfer_value = tacd.value.saturating_mul(helper);
+				T::KtonCurrency::transfer(
+					&con_caller,
+					&wkton_account_id,
+					transfer_value.low_u128().unique_saturated_into(),
+					ExistenceRequirement::AllowDeath,
+				)
+				.map_err(|_| ExitError::Other("Transfer in Kton precompile failed".into()))?;
 
-			// 2. Call wkton sol contract deposit
-			let raw_input = make_deposit_input(input.address, input.value)?;
-			let precompile_address = H160::from_str(KTON_PRECOMPILE).unwrap();
-			T::Runner::call(
-				precompile_address,
-				input.wkton_address,
-				raw_input.to_vec(),
-				U256::zero(),
-				target_limit.unwrap_or_default() as u32,
-				None,
-				None,
-				T::config(),
-			)
-			.map_err(|_| ExitError::Other("Call in Kton precompile failed".into()))?;
+				// 2. Call wkton sol contract deposit
+				let raw_input = make_call_data(tacd.to_address, tacd.value)?;
+				let precompile_address = H160::from_str(KTON_PRECOMPILE).unwrap();
+				T::Runner::call(
+					precompile_address,
+					tacd.wkton_address,
+					raw_input.to_vec(),
+					U256::zero(),
+					target_limit.unwrap_or_default() as u32,
+					None,
+					None,
+					T::config(),
+				)
+				.map_err(|_| ExitError::Other("Call in Kton precompile failed".into()))?;
 
-			Ok((ExitSucceed::Returned, vec![], 20000))
-		} else {
-			Err(ExitError::Other(
-				"Invalid func selector for kton precompile".into(),
-			))
+				Ok((ExitSucceed::Returned, vec![], 20000))
+			}
+			Action::Withdraw(wd) => {
+				let withdraw_value = wd.kton_value.saturating_mul(helper);
+				T::KtonCurrency::transfer(
+					&con_caller,
+					&wd.to_account_id,
+					withdraw_value.low_u128().unique_saturated_into(),
+					ExistenceRequirement::AllowDeath,
+				)
+				.map_err(|_| ExitError::Other("Withdraw in Kton precompile failed".into()))?;
+				Ok((ExitSucceed::Returned, vec![], 20000))
+			}
 		}
 	}
 }
 
-fn make_deposit_input(
+// Action about KTON
+pub enum Action<T: frame_system::Trait> {
+	/// Transfer from substrate account to wkton contract
+	TransferAndCall(CallData),
+	/// Withdraw from wkton contract to substrate account
+	Withdraw(WithdrawData<T>),
+}
+
+// which action depends on the function selector
+pub fn which_action<T: frame_system::Trait>(input_data: &[u8]) -> Result<Action<T>, ExitError> {
+	let transfer_and_call_action = &sha3::Keccak256::digest(&TRANSFER_AND_CALL_ACTION)[0..4];
+	let withdraw_action = &sha3::Keccak256::digest(&WITHDRAW_ACTION)[0..4];
+	if hex::encode(&input_data[0..4]) == hex::encode(transfer_and_call_action) {
+		let decoded_data = CallData::decode(&input_data[4..])?;
+		return Ok(Action::TransferAndCall(decoded_data));
+	} else if hex::encode(&input_data[0..4]) == hex::encode(withdraw_action) {
+		let decoded_data = WithdrawData::decode(&input_data[4..])?;
+		return Ok(Action::Withdraw(decoded_data));
+	}
+	Err(ExitError::Other("Invalid Action！".into()))
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct CallData {
+	wkton_address: H160,
+	to_address: H160,
+	value: U256,
+}
+
+impl CallData {
+	pub fn decode(data: &[u8]) -> Result<Self, ExitError> {
+		let tokens = ethabi::decode(
+			&[ParamType::Address, ParamType::Address, ParamType::Uint(256)],
+			&data,
+		)
+		.map_err(|_| ExitError::Other("ethabi decoded error".into()))?;
+		match (tokens[0].clone(), tokens[1].clone(), tokens[2].clone()) {
+			(
+				Token::Address(eth_wkton_address),
+				Token::Address(eth_to_address),
+				Token::Uint(eth_value),
+			) => {
+				let wkton_address = util::e2s_address(eth_wkton_address);
+				let to_address = util::e2s_address(eth_to_address);
+				let value = util::e2s_u256(eth_value);
+				Ok(CallData {
+					wkton_address,
+					to_address,
+					value,
+				})
+			}
+			_ => Err(ExitError::Other("Invlid call data".into())),
+		}
+	}
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct WithdrawData<T: frame_system::Trait> {
+	pub to_account_id: AccountId<T>,
+	pub kton_value: U256,
+}
+
+impl<T: frame_system::Trait> WithdrawData<T> {
+	pub fn decode(data: &[u8]) -> Result<Self, ExitError> {
+		let tokens = ethabi::decode(&[ParamType::FixedBytes(32), ParamType::Uint(256)], &data)
+			.map_err(|_| ExitError::Other("ethabi decoded error".into()))?;
+		match (tokens[0].clone(), tokens[1].clone()) {
+			(Token::FixedBytes(address), Token::Uint(eth_value)) => Ok(WithdrawData {
+				to_account_id: <T as frame_system::Trait>::AccountId::decode(&mut address.as_ref())
+					.map_err(|_| ExitError::Other("Invalid destination address".into()))?,
+				kton_value: util::e2s_u256(eth_value),
+			}),
+			_ => Err(ExitError::Other("Invlid withdraw input data".into())),
+		}
+	}
+}
+
+fn make_call_data(
 	sp_address: sp_core::H160,
 	sp_value: sp_core::U256,
 ) -> Result<Vec<u8>, ExitError> {
-	// transfer address type
-	let sp_address_bytes = sp_address.to_fixed_bytes();
-	let eth_address = ethereum_types::H160::from_slice(&sp_address_bytes);
-	// transfer value type
-	let mut sp_value_bytes = [0u8; 32];
-	sp_value.to_big_endian(&mut sp_value_bytes);
-	let eth_value = ethereum_types::U256::from_big_endian(&sp_value_bytes);
-	// encode process
+	let eth_address = util::s2e_address(sp_address);
+	let eth_value = util::s2e_u256(sp_value);
 	let func = Function {
 		name: "deposit".to_owned(),
 		inputs: vec![
@@ -125,50 +204,11 @@ fn make_deposit_input(
 		constant: false,
 	};
 	func.encode_input(&[Token::Address(eth_address), Token::Uint(eth_value)])
-		.map_err(|_| ExitError::Other("Make deposit input error happened".into()))
-}
-
-#[derive(Debug, PartialEq, Eq)]
-pub struct InputData {
-	wkton_address: H160,
-	func_selector: [u8; 4],
-	address: H160,
-	value: U256,
-}
-
-impl InputData {
-	pub fn decode(data: &[u8]) -> Result<Self, ExitError> {
-		if data.len() == 76 {
-			let mut wkton_address_bytes = [0u8; 20];
-			wkton_address_bytes.copy_from_slice(&data[0..20]);
-			let wkton_address = H160::from_slice(&wkton_address_bytes);
-
-			let mut func_selector_bytes = [0u8; 4];
-			func_selector_bytes.copy_from_slice(&data[20..24]);
-
-			let mut address_bytes = [0u8; 20];
-			address_bytes.copy_from_slice(&data[24..44]);
-			let address = H160::from_slice(&address_bytes);
-
-			let mut value_bytes = [0u8; 32];
-			value_bytes.copy_from_slice(&data[44..76]);
-			let value = U256::from_big_endian(&value_bytes);
-
-			return Ok(InputData {
-				wkton_address,
-				func_selector: func_selector_bytes,
-				address,
-				value,
-			});
-		}
-		Err(ExitError::Other(
-			"Invalid input data in kton precompile".into(),
-		))
-	}
+		.map_err(|_| ExitError::Other("Make call data error happened".into()))
 }
 
 #[cfg(test)]
-mod testa {
+mod tests {
 	use super::*;
 	use std::str::FromStr;
 
@@ -179,11 +219,11 @@ mod testa {
 		let mock_value_1 = sp_core::U256::from(30);
 		let expected_str = "47e7ef24000000000000000000000000aa01a1bef0557fa9625581a293f3aa7770192632000000000000000000000000000000000000000000000000000000000000001e";
 
-		let encoded_str = hex::encode(make_deposit_input(mock_address, mock_value_1).unwrap());
+		let encoded_str = hex::encode(make_call_data(mock_address, mock_value_1).unwrap());
 		assert_eq!(encoded_str, expected_str);
 
 		let mock_value_2 = sp_core::U256::from(25);
-		let encoded_str = hex::encode(make_deposit_input(mock_address, mock_value_2).unwrap());
+		let encoded_str = hex::encode(make_call_data(mock_address, mock_value_2).unwrap());
 		assert_ne!(encoded_str, expected_str);
 	}
 }
