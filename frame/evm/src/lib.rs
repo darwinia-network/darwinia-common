2b46c0a2119b8e924fe597f487aa5c9282fca4f6
bear <2630582710@qq.com>
2021-03-19 10:47:59 +0800

    Rename module storage prefix (#547)
    * Storage prefix are consistent with updtream lib
    
    * Rename two functions to make code more clearly

--- frame/evm/src/lib.rs
+++ frame/evm/src/lib.rs
@@ -307,7 +307,7 @@ pub struct GenesisAccount {
 }
 
 decl_storage! {
-	trait Store for Module<T: Config> as DarwiniaEVM {
+	trait Store for Module<T: Config> as EVM {
 		pub AccountCodes get(fn account_codes): map hasher(blake2_128_concat) H160 => Vec<u8>;
 		pub AccountStorages get(fn account_storages):
 			double_map hasher(blake2_128_concat) H160, hasher(blake2_128_concat) H256 => H256;
