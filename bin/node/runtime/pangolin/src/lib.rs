dcfec1e0b1baefcede19a4c2f57387bf72547f8c
Xavier Lau <c.estlavie@icloud.com>
2021-03-23 10:46:57 +0800

    More Genesis Validators (#558)
    * update version
    
    * add genesis validators
    
    * some doc

--- bin/node/runtime/pangolin/src/lib.rs
+++ bin/node/runtime/pangolin/src/lib.rs
@@ -397,7 +397,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("Pangolin"),
 	impl_name: create_runtime_str!("Pangolin"),
 	authoring_version: 1,
-	spec_version: 20,
+	// crate version 2.1.0
+	spec_version: 210,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
