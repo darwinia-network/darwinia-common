5507da453128a58ad7dd9ca5559500a92ccf5cab
Xavier Lau <c.estlavie@icloud.com>
2021-03-23 19:00:09 +0800

    add some funds to alice

--- bin/node/cli/src/chain_spec.rs
+++ bin/node/cli/src/chain_spec.rs
@@ -141,7 +141,8 @@ pub fn pangolin_build_spec_config() -> PangolinChainSpec {
 
 fn pangolin_build_spec_genesis() -> pangolin_runtime::GenesisConfig {
 	const ROOT: &'static str = "0x72819fbc1b93196fa230243947c1726cbea7e33044c7eb6f736ff345561f9e4c";
-	const GENESIS_VALIDATOR_BOND: Balance = COIN;
+	const VALIDATOR_BOND: Balance = COIN;
+	const TEAM_MEMBER_RING: Balance = 1_000_000 * COIN;
 
 	struct Keys {
 		stash: AccountId,
@@ -191,14 +192,57 @@ fn pangolin_build_spec_genesis() -> pangolin_runtime::GenesisConfig {
 			"0x1ed7de3855ffcce134d718b570febb49bbbbeb32ebbc8c319f44fb9f5690643a",
 		),
 	];
-	let endowed_accounts = vec![(root.clone(), 1 << 56)]
-		.into_iter()
-		.chain(
-			initial_authorities
-				.iter()
-				.map(|Keys { stash, .. }| (stash.to_owned(), GENESIS_VALIDATOR_BOND)),
+	let team_members = vec![
+		// Huiyi
+		"0x281b7ec1e05feb46457caa9c54cef0ebdaf7f65d31fd6ed740a34dbc9875304c",
+		// Ron
+		"0x9cf0c0ea7488a17e348f0abba9c229032f3240a793ffcfbedc4b46db0aeb306c",
+		// Cheng
+		"0x922b6854052ba1084c74dd323ee70047d58ae4eb068f20bc251831f1ec109030",
+		// Jane
+		"0xb26268877f72c4dcd9c2459a99dde0d2caf5a816c6b4cd3bd1721252b26f4909",
+		// Cai
+		"0xf41d3260d736f5b3db8a6351766e97619ea35972546a5f850bbf0b27764abe03",
+		// Tiny
+		"0xf29638cb649d469c317a4c64381e179d5f64ef4d30207b4c52f2725c9d2ec533",
+		// Eve
+		"0x1a7008a33fa595398b509ef56841db3340931c28a42881e36c9f34b1f15f9271",
+		// Yuqi
+		"0x500e3197e075610c1925ddcd86d66836bf93ae0a476c64f56f611afc7d64d16f",
+		// Aki
+		"0x129f002b1c0787ea72c31b2dc986e66911fe1b4d6dc16f83a1127f33e5a74c7d",
+		// Alex
+		"0x26fe37ba5d35ac650ba37c5cc84525ed135e772063941ae221a1caca192fff49",
+		// Shell
+		"0x187c272f576b1999d6cf3dd529b59b832db12125b43e57fb088677eb0c570a6b",
+		// Echo
+		"0x64766d3a000000000000000f14341a7f464320319025540e8fe48ad0fe5aec8d",
+		// for External Project
+		"0x64766d3a000000000000007682ba569e3823ca1b7317017f5769f8aa8842d461",
+		// Subswap
+		"0x64766d3a00000000000000bb3e51d20ca651fbe19b1a1c2a6c8b1a4d95043703",
+	];
+	let endowed_accounts = vec![
+		(root.clone(), 1 << 56),
+		// Alice
+		(
+			get_account_id_from_seed::<sr25519::Public>("Alice"),
+			1 << 56,
+		),
+	]
+	.into_iter()
+	.chain(
+		initial_authorities
+			.iter()
+			.map(|Keys { stash, .. }| (stash.to_owned(), VALIDATOR_BOND)),
+	)
+	.chain(team_members.into_iter().map(|k| {
+		(
+			array_bytes::hex2array_unchecked!(k, 32).into(),
+			TEAM_MEMBER_RING,
 		)
-		.collect::<Vec<(AccountId, Balance)>>();
+	}))
+	.collect::<Vec<(AccountId, Balance)>>();
 	let evm_accounts = {
 		let mut map = BTreeMap::new();
 
@@ -233,7 +277,7 @@ fn pangolin_build_spec_genesis() -> pangolin_runtime::GenesisConfig {
 				.map(|Keys { stash, .. }| (
 					stash.to_owned(),
 					stash.to_owned(),
-					GENESIS_VALIDATOR_BOND,
+					VALIDATOR_BOND,
 					pangolin_runtime::StakerStatus::Validator
 				))
 				.collect(),
