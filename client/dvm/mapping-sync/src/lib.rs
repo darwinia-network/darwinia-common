948c6e82e194c220036cd60490d22195950ed4b9
ouwenkg <2630582710@qq.com>
2021-04-12 11:47:25 +0800

    Fix issue in error handling, for 341

--- client/dvm/mapping-sync/src/lib.rs
+++ client/dvm/mapping-sync/src/lib.rs
@@ -50,7 +50,7 @@ pub fn sync_block<Block: BlockT>(
 			Ok(())
 		}
 		Err(FindLogError::NotFound) => {
-			let _ = backend.mapping().write_none(header.hash());
+			backend.mapping().write_none(header.hash())?;
 
 			Ok(())
 		}
