cee6922cd0fa24311d4735d5d188c9fa26f837ec
Xavier Lau <c.estlavie@icloud.com>
2021-04-28 03:09:08 +0800

    migrate `tron-backing` to attribute macro

--- frame/bridge/tron/backing/src/lib.rs
+++ frame/bridge/tron/backing/src/lib.rs
@@ -21,76 +21,87 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 pub mod weights;
-// --- darwinia ---
 pub use weights::WeightInfo;
 
-mod types {
+#[frame_support::pallet]
+pub mod pallet {
+	pub mod types {
+		// --- darwinia ---
+		#[cfg(feature = "std")]
+		use crate::pallet::*;
+
+		pub type AccountId<T> = <T as frame_system::Config>::AccountId;
+
+		#[cfg(feature = "std")]
+		pub type RingBalance<T> = <RingCurrency<T> as Currency<AccountId<T>>>::Balance;
+		#[cfg(feature = "std")]
+		pub type KtonBalance<T> = <KtonCurrency<T> as Currency<AccountId<T>>>::Balance;
+
+		#[cfg(feature = "std")]
+		type RingCurrency<T> = <T as Config>::RingCurrency;
+		#[cfg(feature = "std")]
+		type KtonCurrency<T> = <T as Config>::KtonCurrency;
+	}
+	pub use types::*;
+
+	// --- substrate ---
+	use frame_support::{
+		pallet_prelude::*,
+		traits::{Currency, Get},
+	};
+	use frame_system::pallet_prelude::*;
+	use sp_runtime::{traits::AccountIdConversion, ModuleId};
 	// --- darwinia ---
+	use crate::weights::WeightInfo;
+
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		// --- substrate ---
+		type WeightInfo: WeightInfo;
+		// --- darwinia ---
+		#[pallet::constant]
+		type ModuleId: Get<ModuleId>;
+		type RingCurrency: Currency<AccountId<Self>>;
+		type KtonCurrency: Currency<AccountId<Self>>;
+	}
+
+	#[pallet::genesis_config]
+	pub struct GenesisConfig<T: Config> {
+		pub backed_ring: RingBalance<T>,
+		pub backed_kton: KtonBalance<T>,
+	}
 	#[cfg(feature = "std")]
-	use crate::*;
+	impl<T: Config> Default for GenesisConfig<T> {
+		fn default() -> Self {
+			GenesisConfig {
+				backed_ring: Default::default(),
+				backed_kton: Default::default(),
+			}
+		}
+	}
+	#[pallet::genesis_build]
+	impl<T: Config> GenesisBuild<T> for GenesisConfig<T> {
+		fn build(&self) {
+			let module_account = <Pallet<T>>::account_id();
 
-	pub type AccountId<T> = <T as frame_system::Config>::AccountId;
-
-	#[cfg(feature = "std")]
-	pub type RingBalance<T> = <RingCurrency<T> as Currency<AccountId<T>>>::Balance;
-	#[cfg(feature = "std")]
-	pub type KtonBalance<T> = <KtonCurrency<T> as Currency<AccountId<T>>>::Balance;
-
-	#[cfg(feature = "std")]
-	type RingCurrency<T> = <T as Config>::RingCurrency;
-	#[cfg(feature = "std")]
-	type KtonCurrency<T> = <T as Config>::KtonCurrency;
-}
-
-// --- substrate ---
-use frame_support::{
-	decl_module, decl_storage,
-	traits::{Currency, Get},
-};
-use sp_runtime::{traits::AccountIdConversion, ModuleId};
-// --- darwinia ---
-use types::*;
-
-pub trait Config: frame_system::Config {
-	type ModuleId: Get<ModuleId>;
-
-	type RingCurrency: Currency<AccountId<Self>>;
-	type KtonCurrency: Currency<AccountId<Self>>;
-
-	type WeightInfo: WeightInfo;
-}
-
-decl_storage! {
-	trait Store for Module<T: Config> as DarwiniaTronBacking {}
-
-	add_extra_genesis {
-		config(backed_ring): RingBalance<T>;
-		config(backed_kton): KtonBalance<T>;
-		build(|config| {
-			let module_account = <Module<T>>::account_id();
 			let _ = T::RingCurrency::make_free_balance_be(
 				&module_account,
-				T::RingCurrency::minimum_balance() + config.backed_ring
+				T::RingCurrency::minimum_balance() + self.backed_ring,
 			);
-			let _ = T::KtonCurrency::make_free_balance_be(
-				&module_account,
-				config.backed_kton
-			);
-		});
+			let _ = T::KtonCurrency::make_free_balance_be(&module_account, self.backed_kton);
+		}
 	}
-}
 
-decl_module! {
-	pub struct Module<T: Config> for enum Call
-	where
-		origin: T::Origin
-	{
-		const ModuleId: ModuleId = T::ModuleId::get();
+	#[pallet::pallet]
+	pub struct Pallet<T>(_);
+	#[pallet::hooks]
+	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {}
+	impl<T: Config> Pallet<T> {
+		pub fn account_id() -> T::AccountId {
+			T::ModuleId::get().into_account()
+		}
 	}
 }
-
-impl<T: Config> Module<T> {
-	pub fn account_id() -> T::AccountId {
-		T::ModuleId::get().into_account()
-	}
-}
+pub use pallet::*;
