b1c12e40114c0513aa507e8661b6387f5eb827d4
Xavier Lau <c.estlavie@icloud.com>
2021-05-13 00:15:49 +0800

    companion for #substrate-8414

--- frame/staking/src/mock.rs
+++ frame/staking/src/mock.rs
@@ -840,7 +840,7 @@ pub(crate) fn on_offence_in_era(
 	let bonded_eras = BondedEras::get();
 	for &(bonded_era, start_session) in bonded_eras.iter() {
 		if bonded_era == era {
-			let _ = Staking::on_offence(offenders, slash_fraction, start_session).unwrap();
+			let _ = Staking::on_offence(offenders, slash_fraction, start_session);
 			return;
 		} else if bonded_era > era {
 			break;
@@ -852,8 +852,7 @@ pub(crate) fn on_offence_in_era(
 			offenders,
 			slash_fraction,
 			Staking::eras_start_session_index(era).unwrap(),
-		)
-		.unwrap();
+		);
 	} else {
 		panic!("cannot slash in era {}", era);
 	}
