6bd239c6893e78c68a01ed7bfc92bccdef0cee1d
ouwenkg <2630582710@qq.com>
2021-05-18 17:54:36 +0800

    Companion for 337

--- frame/evm/precompile/contracts/bn128/src/lib.rs
+++ frame/evm/precompile/contracts/bn128/src/lib.rs
@@ -20,8 +20,8 @@
 extern crate alloc;
 
 use alloc::vec::Vec;
-use dp_evm::LinearCostPrecompile;
-use evm::{ExitError, ExitSucceed};
+use dp_evm::Precompile;
+use evm::{Context, ExitError, ExitSucceed};
 use sp_core::U256;
 
 fn read_fr(input: &[u8], start_inx: usize) -> Result<bn::Fr, ExitError> {
@@ -48,11 +48,16 @@ fn read_point(input: &[u8], start_inx: usize) -> Result<bn::G1, ExitError> {
 /// The Bn128Add builtin
 pub struct Bn128Add;
 
-impl LinearCostPrecompile for Bn128Add {
-	const BASE: u64 = 15;
-	const WORD: u64 = 3;
+impl Bn128Add {
+	const GAS_COST: u64 = 150; // https://eips.ethereum.org/EIPS/eip-1108
+}
 
-	fn execute(input: &[u8], _: u64) -> core::result::Result<(ExitSucceed, Vec<u8>), ExitError> {
+impl Precompile for Bn128Add {
+	fn execute(
+		input: &[u8],
+		_target_gas: Option<u64>,
+		_context: &Context,
+	) -> core::result::Result<(ExitSucceed, Vec<u8>, u64), ExitError> {
 		use bn::AffineG1;
 
 		let p1 = read_point(input, 0)?;
@@ -69,18 +74,23 @@ impl LinearCostPrecompile for Bn128Add {
 			})?;
 		}
 
-		Ok((ExitSucceed::Returned, buf.to_vec()))
+		Ok((ExitSucceed::Returned, buf.to_vec(), Bn128Add::GAS_COST))
 	}
 }
 
 /// The Bn128Mul builtin
 pub struct Bn128Mul;
 
-impl LinearCostPrecompile for Bn128Mul {
-	const BASE: u64 = 15;
-	const WORD: u64 = 3;
+impl Bn128Mul {
+	const GAS_COST: u64 = 6_000; // https://eips.ethereum.org/EIPS/eip-1108
+}
 
-	fn execute(input: &[u8], _: u64) -> core::result::Result<(ExitSucceed, Vec<u8>), ExitError> {
+impl Precompile for Bn128Mul {
+	fn execute(
+		input: &[u8],
+		_target_gas: Option<u64>,
+		_context: &Context,
+	) -> core::result::Result<(ExitSucceed, Vec<u8>, u64), ExitError> {
 		use bn::AffineG1;
 
 		let p = read_point(input, 0)?;
@@ -97,25 +107,41 @@ impl LinearCostPrecompile for Bn128Mul {
 			})?;
 		}
 
-		Ok((ExitSucceed::Returned, buf.to_vec()))
+		Ok((ExitSucceed::Returned, buf.to_vec(), Bn128Mul::GAS_COST))
 	}
 }
 
 /// The Bn128Pairing builtin
 pub struct Bn128Pairing;
 
-impl LinearCostPrecompile for Bn128Pairing {
-	const BASE: u64 = 15;
-	const WORD: u64 = 3;
+impl Bn128Pairing {
+	// https://eips.ethereum.org/EIPS/eip-1108
+	const BASE_GAS_COST: u64 = 45_000;
+	const GAS_COST_PER_PAIRING: u64 = 34_000;
+}
 
-	fn execute(input: &[u8], _: u64) -> core::result::Result<(ExitSucceed, Vec<u8>), ExitError> {
+impl Precompile for Bn128Pairing {
+	fn execute(
+		input: &[u8],
+		target_gas: Option<u64>,
+		_context: &Context,
+	) -> core::result::Result<(ExitSucceed, Vec<u8>, u64), ExitError> {
 		use bn::{pairing_batch, AffineG1, AffineG2, Fq, Fq2, Group, Gt, G1, G2};
 
-		let ret_val = if input.is_empty() {
-			U256::one()
+		let (ret_val, gas_cost) = if input.is_empty() {
+			(U256::one(), Bn128Pairing::BASE_GAS_COST)
 		} else {
 			// (a, b_a, b_b - each 64-byte affine coordinates)
 			let elements = input.len() / 192;
+
+			let gas_cost: u64 = Bn128Pairing::BASE_GAS_COST
+				+ (elements as u64 * Bn128Pairing::GAS_COST_PER_PAIRING);
+			if let Some(gas_left) = target_gas {
+				if gas_left < gas_cost {
+					return Err(ExitError::OutOfGas);
+				}
+			}
+
 			let mut vals = Vec::new();
 			for idx in 0..elements {
 				let a_x = Fq::from_slice(&input[idx * 192..idx * 192 + 32])
@@ -166,15 +192,15 @@ impl LinearCostPrecompile for Bn128Pairing {
 			let mul = pairing_batch(&vals);
 
 			if mul == Gt::one() {
-				U256::one()
+				(U256::one(), gas_cost)
 			} else {
-				U256::zero()
+				(U256::zero(), gas_cost)
 			}
 		};
 
 		let mut buf = [0u8; 32];
 		ret_val.to_big_endian(&mut buf);
 
-		Ok((ExitSucceed::Returned, buf.to_vec()))
+		Ok((ExitSucceed::Returned, buf.to_vec(), gas_cost))
 	}
 }
