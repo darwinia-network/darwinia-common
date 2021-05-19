862c65b959db873a5652e9d78b0d3732dc26957e
ouwenkg <2630582710@qq.com>
2021-05-19 15:06:01 +0800

    Companion for 375,379

--- frame/dvm/src/lib.rs
+++ frame/dvm/src/lib.rs
@@ -211,12 +211,21 @@ impl<T: Config> frame_support::unsigned::ValidateUnsigned for Module<T> {
 				return InvalidTransaction::Payment.into();
 			}
 
-			if transaction.gas_price < T::FeeCalculator::min_gas_price() {
+			let min_gas_price = T::FeeCalculator::min_gas_price();
+
+			if transaction.gas_price < min_gas_price {
 				return InvalidTransaction::Payment.into();
 			}
 
-			let mut builder =
-				ValidTransactionBuilder::default().and_provides((origin, transaction.nonce));
+			let mut builder = ValidTransactionBuilder::default()
+				.and_provides((origin, transaction.nonce))
+				.priority(if min_gas_price == U256::zero() {
+					0
+				} else {
+					let target_gas =
+						(transaction.gas_limit * transaction.gas_price) / min_gas_price;
+					T::GasWeightMapping::gas_to_weight(target_gas.unique_saturated_into())
+				});
 
 			if transaction.nonce > account_data.nonce {
 				if let Some(prev_nonce) = transaction.nonce.checked_sub(1.into()) {
