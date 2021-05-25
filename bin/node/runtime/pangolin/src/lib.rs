86c2667f12282cc01064b72b9b9bf1bf3d8d313b
bear <wpf15701504487@gmail.com>
2021-05-25 09:02:30 +0800

    Remove Event `TargetMinGasPriceSet` (#642)
    * Remove event
    
    * Move kill() to on_initialize
    
    * Update spec version
    
    * correct weight
    
    Co-authored-by: Xavier Lau <c.estlavie@icloud.com>

--- bin/node/runtime/pangolin/src/lib.rs
+++ bin/node/runtime/pangolin/src/lib.rs
@@ -270,7 +270,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	impl_name: sp_runtime::create_runtime_str!("Pangolin"),
 	authoring_version: 1,
 	// crate version ~2.4.0 := >=2.4.0, <2.5.0
-	spec_version: 2401,
+	spec_version: 2402,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
