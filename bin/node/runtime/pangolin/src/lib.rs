17205d35e24e5a65599ba1ba23e2c13513d36414
Xavier Lau <hi@xavier.zone>
2021-06-28 17:48:25 +0800

    Release v2.5.0 (#678)

--- bin/node/runtime/pangolin/src/lib.rs
+++ bin/node/runtime/pangolin/src/lib.rs
@@ -271,8 +271,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: sp_runtime::create_runtime_str!("Pangolin"),
 	impl_name: sp_runtime::create_runtime_str!("Pangolin"),
 	authoring_version: 1,
-	// crate version ~2.4.0 := >=2.4.0, <2.5.0
-	spec_version: 2403,
+	// crate version ~2.5.0 := >=2.4.0, <2.6.0
+	spec_version: 2500,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
