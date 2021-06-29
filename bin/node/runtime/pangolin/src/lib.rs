4f8a9fed4b04c1789d06441bb32da6ba0780e09f
Xavier Lau <hi@xavier.zone>
2021-06-30 00:42:44 +0800

    Correct Comment

--- bin/node/runtime/pangolin/src/lib.rs
+++ bin/node/runtime/pangolin/src/lib.rs
@@ -271,7 +271,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: sp_runtime::create_runtime_str!("Pangolin"),
 	impl_name: sp_runtime::create_runtime_str!("Pangolin"),
 	authoring_version: 1,
-	// crate version ~2.5.0 := >=2.4.0, <2.6.0
+	// crate version ~2.5.0 := >=2.5.0, <2.6.0
 	spec_version: 2501,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
