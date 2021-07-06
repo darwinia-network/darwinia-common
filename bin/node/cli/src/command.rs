0d5bc61b80c57ca20b213412380b1ddd15e34ac1
bear <wpf15701504487@gmail.com>
2021-07-06 17:45:52 +0800

    merge s2s to main (#694)
    * Serde for `std` (#688)
    
    * Benchmark framework for runtime (#687)
    
    * Add the frameworks
    
    * Rm test scripts
    
    * Rm frame/s2s
    
    * Update dispatch_benchmark
    
    * Fix compile complain
    
    * Format works
    
    * Chores
    
    * Remove useless comments
    
    * Try fix js ci test
    
    * Reorder toml
    
    * simplify uri
    
    * shorten delay
    
    Co-authored-by: Xavier Lau <hi@xavier.zone>
    
    * change s2s to main for all dependencites
    
    * Fix compile issue
    
    Co-authored-by: Xavier Lau <hi@xavier.zone>

--- bin/node/cli/src/command.rs
+++ bin/node/cli/src/command.rs
@@ -185,6 +185,19 @@ pub fn run() -> sc_cli::Result<()> {
 				Ok((cmd.run(client, backend), task_manager))
 			})
 		}
+		Some(Subcommand::Benchmark(cmd)) => {
+			if cfg!(feature = "runtime-benchmarks") {
+				let runner = cli.create_runner(cmd)?;
+
+				runner.sync_run(|config| {
+					cmd.run::<service::pangolin_runtime::Block, service::PangolinExecutor>(config)
+				})
+			} else {
+				Err("Benchmarking wasn't enabled when building the node. \
+				You can enable it with `--features runtime-benchmarks`."
+					.into())
+			}
+		}
 		Some(Subcommand::Key(cmd)) => cmd.run(&cli),
 		Some(Subcommand::Sign(cmd)) => cmd.run(),
 		Some(Subcommand::Verify(cmd)) => cmd.run(),
