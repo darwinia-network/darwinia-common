909244613205f0b1b8f06c79c922a18e724d107e
ouwenkg <2630582710@qq.com>
2021-07-09 15:03:48 +0800

    companion for 404, Remove frontier offchain db on purge-chain subcommand

--- bin/node/cli/src/service.rs
+++ bin/node/cli/src/service.rs
@@ -176,7 +176,7 @@ fn set_prometheus_registry(config: &mut Configuration) -> Result<(), ServiceErro
 	Ok(())
 }
 
-fn open_frontier_backend(config: &Configuration) -> Result<Arc<Backend<Block>>, String> {
+pub fn dvm_database_dir(config: &Configuration) -> std::path::PathBuf {
 	let config_dir = config
 		.base_path
 		.as_ref()
@@ -185,11 +185,13 @@ fn open_frontier_backend(config: &Configuration) -> Result<Arc<Backend<Block>>, 
 			BasePath::from_project("", "", &crate::cli::Cli::executable_name())
 				.config_dir(config.chain_spec.id())
 		});
-	let database_dir = config_dir.join("dvm").join("db");
+	config_dir.join("dvm").join("db")
+}
 
+pub fn open_dvm_backend(config: &Configuration) -> Result<Arc<Backend<Block>>, String> {
 	Ok(Arc::new(Backend::<Block>::new(&DatabaseSettings {
 		source: DatabaseSettingsSrc::RocksDb {
-			path: database_dir,
+			path: dvm_database_dir(&config),
 			cache_size: 0,
 		},
 	})?))
@@ -274,7 +276,7 @@ where
 		task_manager.spawn_handle(),
 		client.clone(),
 	);
-	let frontier_backend = open_frontier_backend(config)?;
+	let frontier_backend = open_dvm_backend(config)?;
 	let grandpa_hard_forks = vec![];
 	let (grandpa_block_import, grandpa_link) =
 		sc_finality_grandpa::block_import_with_authority_set_hard_forks(
