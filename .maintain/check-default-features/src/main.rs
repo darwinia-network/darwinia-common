4656121ebf155ced1ca50c9e3d6baf3b4a828c12
Xavier Lau <hi@xavier.zone>
2021-07-16 04:06:33 +0800

    CI checks `default-features`

---
+++ .maintain/check-default-features/src/main.rs
@@ -0,0 +1,54 @@
+// --- std ---
+use std::{env, process};
+// --- crates.io ---
+use cargo_toml::Manifest;
+use walkdir::WalkDir;
+
+fn main() {
+	let mut incomplete_dependencies = vec![];
+
+	for e in WalkDir::new(env::current_dir().unwrap())
+		.into_iter()
+		.filter_entry(|e| {
+			let n = e.file_name().to_str().unwrap();
+
+			n != "target" && !(n.starts_with('.') && !n.starts_with("./"))
+		})
+		.filter_map(|e| e.ok())
+	{
+		if e.file_name() == "Cargo.toml" {
+			let manifest = Manifest::from_path(e.path()).unwrap();
+
+			if let Some(std) = manifest.features.get("std") {
+				for (alias, dependency) in manifest.dependencies.iter() {
+					if let Some(detail) = dependency.detail() {
+						if let Some(default_features) = detail.default_features {
+							if !default_features {
+								if !std.contains(&format!("{}/std", alias)) {
+									incomplete_dependencies.push((
+										alias.to_owned(),
+										e.path()
+											.to_str()
+											.unwrap()
+											.split("common/")
+											.last()
+											.unwrap()
+											.to_owned(),
+									));
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	if !incomplete_dependencies.is_empty() {
+		for (alias, path) in incomplete_dependencies {
+			println!("Incomplete std feature found for `{}` at {}", alias, path);
+		}
+
+		process::exit(1);
+	}
+}
