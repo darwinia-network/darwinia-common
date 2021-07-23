085d610fac3410ad050c3bfc3cf0a85d47d0a3cc
ouwenkg <2630582710@qq.com>
2021-07-23 15:09:17 +0800

    Add benchmark file

--- bench-evm.sh
+++ bench-evm.sh
@@ -1,15 +1,15 @@
-
 #!/bin/bash
 
 RUST_LOG=darwinia_evm=debug ./target/release/drml benchmark \
   --chain dev \
-  --wasm-execution compiled \
   --pallet darwinia_evm \
   --execution wasm \
-  --extrinsic=* \
-  --steps 100 \
-  --repeat 10 \
+  --wasm-execution compiled \
+  --extrinsic=runner_execute \
+  --steps 5 \
+  --repeat 2 \
   --raw \
   --heap-pages=4096 \
-  --output=./frame/bridge/s2s/issuing/src/weight.rs \
+  --output=./frame/evm/src/weight.rs \
   --template=./.maintain/frame-weight-template.hbs
+
