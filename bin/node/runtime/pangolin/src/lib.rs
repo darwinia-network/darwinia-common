b1750f6af67a84d4edd45fc0d8eae15de8528afb
Xavier Lau <hi@xavier.zone>
2021-08-18 16:19:35 +0800

    update `transaction_version`

--- bin/node/runtime/pangolin/src/lib.rs
+++ bin/node/runtime/pangolin/src/lib.rs
@@ -294,7 +294,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_version: 2600,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 1,
+	transaction_version: 2,
 };
 
 /// The BABE epoch configuration at genesis.
