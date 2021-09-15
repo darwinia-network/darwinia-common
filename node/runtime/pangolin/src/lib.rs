c38ac178b8c50646345f836c9f1647c38c29148a
Xavier Lau <hi@xavier.zone>
2021-09-15 14:59:56 +0800

    Bump `spec_version`

--- node/runtime/pangolin/src/lib.rs
+++ node/runtime/pangolin/src/lib.rs
@@ -168,7 +168,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: sp_runtime::create_runtime_str!("Pangolin"),
 	impl_name: sp_runtime::create_runtime_str!("Pangolin"),
 	authoring_version: 1,
-	spec_version: 2630,
+	spec_version: 2640,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 4,
