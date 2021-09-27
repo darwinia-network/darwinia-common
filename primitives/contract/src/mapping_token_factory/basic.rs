2c42e10d5df295e988e03821b019a4de8af87131
xiaocheng <xiaoch2010@gmail.com>
2021-09-27 22:51:42 +0800

    contract encoder/decoder

---
+++ primitives/contract/src/mapping_token_factory/basic.rs
@@ -0,0 +1,163 @@
+// This file is part of Darwinia.
+//
+// Copyright (C) 2018-2021 Darwinia Network
+// SPDX-License-Identifier: GPL-3.0
+//
+// Darwinia is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Darwinia is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Darwinia. If not, see <https://www.gnu.org/licenses/>.
+
+//! basic mapping token factory encode and decode.
+
+pub use ethabi::{Event, Log};
+
+// --- crates.io ---
+use ethereum_types::{Address as EthereumAddress, H160, U256};
+// --- darwinia-network ---
+use ethabi::{
+	param_type::ParamType, token::Token, Bytes, Error, Function, Param, Result as AbiResult,
+};
+// --- paritytech ---
+use sp_std::prelude::*;
+
+pub struct BasicMappingTokenFactory;
+
+impl BasicMappingTokenFactory {
+	/// encode get mapping token info function
+	pub fn encode_mapping_token(
+		backing_address: EthereumAddress,
+		original_token: EthereumAddress,
+	) -> AbiResult<Bytes> {
+		let inputs = vec![
+			Param {
+				name: "backing_address".into(),
+				kind: ParamType::Address,
+			},
+			Param {
+				name: "original_token".into(),
+				kind: ParamType::Address,
+			},
+		];
+
+		let outputs = vec![Param {
+			name: "mapping_token".into(),
+			kind: ParamType::Address,
+		}];
+
+		Function {
+			name: "mappingToken".into(),
+			inputs,
+			outputs,
+			constant: true,
+		}.encode_input(
+            vec![
+            Token::Address(backing_address),
+            Token::Address(original_token)
+            ].as_slice())
+	}
+
+    /// encode create new erc20 function
+	pub fn encode_create_erc20(
+		token_type: u32,
+		name: &str,
+		symbol: &str,
+		decimals: u8,
+		backing_address: EthereumAddress,
+		original_token: EthereumAddress,
+	) -> AbiResult<Bytes> {
+		let inputs = vec![
+			Param {
+				name: "tokenType".into(),
+				kind: ParamType::Uint(32),
+			},
+			Param {
+				name: "name".into(),
+				kind: ParamType::String,
+			},
+			Param {
+				name: "symbol".into(),
+				kind: ParamType::String,
+			},
+			Param {
+				name: "decimals".into(),
+				kind: ParamType::Uint(8),
+			},
+			Param {
+				name: "backing_address".into(),
+				kind: ParamType::Address,
+			},
+			Param {
+				name: "original_token".into(),
+				kind: ParamType::Address,
+			},
+		];
+
+		let outputs = vec![Param {
+			name: "token".into(),
+			kind: ParamType::Address,
+		}];
+
+		Function {
+			name: "newErc20Contract".into(),
+			inputs,
+			outputs,
+			constant: false,
+		}.encode_input(
+			vec![
+				Token::FixedBytes(event_receiver.to_vec()),
+				Token::Uint(U256::from(token_type)),
+				Token::String(name.into()),
+				Token::String(symbol.into()),
+				Token::Uint(U256::from(decimals)),
+				Token::Address(backing),
+				Token::Address(source),
+			]
+			.as_slice(),
+        )
+	}
+
+    /// encode issuing function for erc20
+	pub fn encode_issue_erc20(
+		token: EthereumAddress,
+		recipient: EthereumAddress,
+		amount: U256,
+	) -> AbiResult<Bytes> {
+		let inputs = vec![
+			Param {
+				name: "mapping_token".into(),
+				kind: ParamType::Address,
+			},
+			Param {
+				name: "recipient".into(),
+				kind: ParamType::Address,
+			},
+			Param {
+				name: "amount".into(),
+				kind: ParamType::Uint(256),
+			},
+		];
+
+		Function {
+			name: "issueMappingToken".into(),
+			inputs,
+			outputs: vec![],
+			constant: false,
+		}.encode_input(
+			vec![
+				Token::Address(token),
+				Token::Address(recipient),
+				Token::Uint(amount),
+			]
+			.as_slice(),
+		)
+	}
+}
---
+++ primitives/contract/src/mapping_token_factory/s2s.rs
@@ -0,0 +1,141 @@
+// This file is part of Darwinia.
+//
+// Copyright (C) 2018-2021 Darwinia Network
+// SPDX-License-Identifier: GPL-3.0
+//
+// Darwinia is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Darwinia is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Darwinia. If not, see <https://www.gnu.org/licenses/>.
+
+//! substrate to substrate mapping token factory encode and decode.
+
+pub use ethabi::{Event, Log};
+
+// --- crates.io ---
+use ethereum_types::{Address as EthereumAddress, H160, U256};
+// --- darwinia-network ---
+use ethabi::{
+	param_type::ParamType, token::Token, Bytes, Error, Function, Param, Result as AbiResult,
+};
+// --- paritytech ---
+use sp_std::prelude::*;
+
+pub struct Sub2SubMappingTokenFactory;
+
+impl Sub2SubMappingTokenFactory {
+	/// encode confirm burn and remote unlock deliver message function
+	pub fn encode_confirm_burn_and_remote_unlock(
+		message_id: Vec<u8>,
+		result: bool,
+	) -> AbiResult<Bytes> {
+		let inputs = vec![
+			Param {
+				name: "messageId".into(),
+				kind: ParamType::Bytes,
+			},
+			Param {
+				name: "result".into(),
+				kind: ParamType::Bool,
+			},
+		];
+
+		Function {
+			name: "confirmBurnAndRemoteUnlock".into(),
+			inputs,
+			outputs: vec![],
+			constant: false,
+		}.encode_input(
+            vec![
+                Token::Bytes(message_id),
+                Token::Bool(result)
+            ].as_slice(),
+        )
+	}
+}
+
+/// S2sRemoteUnlockInfo
+/// this is the unlock message from mapping-token-factory after some user burn their mapped token
+/// and would be sent to the outer chain to unlock the original token
+/// @spec_version: the remote chain's spec_version
+/// @weight: the remote dispatch call's weight
+/// @token_type: the type of original_token(native, erc20, etc)
+/// @original_token: the origin token address
+/// @recipient: the final receiver of the token to be unlocked on the source chain
+/// @amount: the amount of the unlocked token
+#[derive(Debug, PartialEq, Eq)]
+pub struct S2sRemoteUnlockInfo {
+	pub spec_version: u32,
+	pub weight: u64,
+	pub token_type: u32,
+	pub original_token: H160,
+	pub recipient: Vec<u8>,
+	pub amount: U256,
+}
+
+impl S2sRemoteUnlockInfo {
+	pub fn encode(
+		spec_version: u32,
+		weight: u64,
+		token_type: u32,
+		original_token: H160,
+		recipient: Vec<u8>,
+		amount: U256,
+	) -> Vec<u8> {
+		ethabi::encode(&[
+			Token::Uint(spec_version.into()),
+			Token::Uint(weight.into()),
+			Token::Uint(token_type.into()),
+			Token::Address(original_token),
+			Token::Bytes(recipient),
+			Token::Uint(amount),
+		])
+	}
+
+	pub fn decode(data: &[u8]) -> AbiResult<Self> {
+		let tokens = ethabi::decode(
+			&[
+				ParamType::Uint(256),
+				ParamType::Uint(256),
+				ParamType::Uint(256),
+				ParamType::Address,
+				ParamType::Bytes,
+				ParamType::Uint(256),
+			],
+			&data,
+		)?;
+		match (
+			tokens[0].clone(),
+			tokens[1].clone(),
+			tokens[2].clone(),
+			tokens[3].clone(),
+			tokens[4].clone(),
+			tokens[5].clone(),
+		) {
+			(
+				Token::Uint(spec_version),
+				Token::Uint(weight),
+				Token::Uint(token_type),
+				Token::Address(original_token),
+				Token::Bytes(recipient),
+				Token::Uint(amount),
+			) => Ok(TokenBurnInfo {
+				spec_version: spec_version.low_u32(),
+				weight: weight.low_u64(),
+				token_type: token_type.low_u32(),
+				original_token,
+				recipient,
+				amount,
+			}),
+			_ => Err(Error::InvalidData),
+		}
+	}
+}
