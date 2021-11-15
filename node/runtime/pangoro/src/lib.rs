118dfb542441aa6e474c55eb238bacaaca2f91e2
Xavier Lau <hi@xavier.zone>
2021-11-15 16:21:21 +0800

    Format

--- node/runtime/pangoro/src/lib.rs
+++ node/runtime/pangoro/src/lib.rs
@@ -135,7 +135,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: sp_runtime::create_runtime_str!("Pangoro"),
 	impl_name: sp_runtime::create_runtime_str!("Pangoro"),
 	authoring_version: 1,
-	spec_version: 26100,
+	spec_version: 2_6_10_0,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 3,
