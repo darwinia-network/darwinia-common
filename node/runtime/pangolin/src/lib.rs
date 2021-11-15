931b471794cab9de63744ad230ac16420d866f8d
Xavier Lau <hi@xavier.zone>
2021-11-15 17:00:09 +0800

    Release v2.6.10 (#927)
    * Bump Specification Version
    
    * Bump Crate Version
    
    * Format

--- node/runtime/pangolin/src/lib.rs
+++ node/runtime/pangolin/src/lib.rs
@@ -171,7 +171,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: sp_runtime::create_runtime_str!("Pangolin"),
 	impl_name: sp_runtime::create_runtime_str!("Pangolin"),
 	authoring_version: 1,
-	spec_version: 2690,
+	spec_version: 2_6_10_0,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 4,
