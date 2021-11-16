82a281d53c0812df22b6d9ea509234c90425c99b
ouwenkg <2630582710@qq.com>
2021-11-16 14:14:11 +0800

    Update `AssignedRelayersNumber` in pangoro

--- node/runtime/pangoro/src/pallets/fee_market.rs
+++ node/runtime/pangoro/src/pallets/fee_market.rs
@@ -10,7 +10,7 @@ frame_support::parameter_types! {
 	pub const TreasuryPalletId: PalletId = PalletId(*b"da/trsry");
 	pub const FeeMarketLockId: LockIdentifier = *b"da/feelf";
 
-	pub const AssignedRelayersNumber: u64 = 3;
+	pub const AssignedRelayersNumber: u64 = 1;
 	pub const MinimumRelayFee: Balance = 15 * COIN;
 	pub const CollateralPerOrder: Balance = 100 * COIN;
 	pub const SlashPerBlock: Balance = 2 * COIN;
