b2dce1a756458fc815fc9c278a37aa52c2bbdb62
Xavier Lau <xavier@inv.cafe>
2021-11-26 15:16:55 +0800

    Release v2.7.0 (#957)
    * Update Version
    
    * Update Runtime Version

--- node/runtime/pangolin/src/lib.rs
+++ node/runtime/pangolin/src/lib.rs
@@ -170,11 +170,11 @@ type Ring = Balances;
 pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: sp_runtime::create_runtime_str!("Pangolin"),
 	impl_name: sp_runtime::create_runtime_str!("Pangolin"),
-	authoring_version: 1,
-	spec_version: 2_6_10_0,
-	impl_version: 1,
+	authoring_version: 0,
+	spec_version: 2_7_00_0,
+	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 4,
+	transaction_version: 0,
 };
 
 /// The BABE epoch configuration at genesis.
