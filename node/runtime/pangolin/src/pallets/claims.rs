3dc6f0f26382b8e644c2cf699f83ba6d0b61b03f
Xavier Lau <xavier@inv.cafe>
2021-11-29 11:38:50 +0800

    Fix Global Replace Issues

--- node/runtime/pangolin/src/pallets/claims.rs
+++ node/runtime/pangolin/src/pallets/claims.rs
@@ -6,7 +6,7 @@ use darwinia_claims::Config;
 
 frame_support::parameter_types! {
 	pub const ClaimsPalletId: PalletId = PalletId(*b"da/claim");
-	pub Prefix: &'static [u8] = b"Pay RINGs to the Crab account:";
+	pub Prefix: &'static [u8] = b"Pay PRINGs to the Pangolin account:";
 }
 
 impl Config for Runtime {
