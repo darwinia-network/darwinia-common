4773e55e8997239fbb45f30d27c37d17ca406eec
Xavier Lau <xavier@inv.cafe>
2021-11-29 18:01:15 +0800

    Purge Large Cache

---
+++ .maintain/purge-large-cache.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cargo clean -p drml 2> /dev/null || true
+cargo clean -p drml-service 2> /dev/null || true
+cargo clean -p pangoro-runtime 2> /dev/null || true
+cargo clean -p pangolin-runtime 2> /dev/null || true
+cargo clean -p librocksdb-sys 2> /dev/null || true
+rm -rf target/debug/wbuild 2> /dev/null || true
