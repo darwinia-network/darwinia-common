f44cf18097adfd19b804f5a60ad0d37403a0d0ec
Xavier Lau <xavier@inv.cafe>
2021-11-30 11:00:55 +0800

    Update Purge Items

--- .maintain/purge-large-cache.sh
+++ .maintain/purge-large-cache.sh
@@ -4,5 +4,4 @@ cargo clean -p drml 2> /dev/null || true
 cargo clean -p drml-service 2> /dev/null || true
 cargo clean -p pangoro-runtime 2> /dev/null || true
 cargo clean -p pangolin-runtime 2> /dev/null || true
-cargo clean -p librocksdb-sys 2> /dev/null || true
 rm -rf target/debug/wbuild 2> /dev/null || true
