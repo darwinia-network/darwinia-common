cb2fc600d31def009e337b2c2cff99e56f596af4
Xavier Lau <xavier@inv.cafe>
2021-12-02 12:51:08 +0800

    Use `127.0.0.1` If Someone Did Not Configurate Localhost

--- tests/dvm/tests/contract/test-constract-method.js
+++ tests/dvm/tests/contract/test-constract-method.js
@@ -6,7 +6,7 @@ const Web3 = require("web3");
 const contractFile = require("./compile");
 const conf = require("../config.js");
 
-const web3 = new Web3("http://localhost:9933");
+const web3 = new Web3("http://127.0.0.1:9933");
 const address = "0x6Be02d1d3665660d22FF9624b7BE0551ee1Ac91b";
 const privKey = "99B3C12287537E38C90A9219D4CB074A89A16E9CDB20BF85728EBD97C343E342"; // Genesis private key
 const bytecode = contractFile.evm.bytecode.object;
