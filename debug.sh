9ca40ef6d2aed9b9c10e60704d621227c67cf929
ouwenkg <2630582710@qq.com>
2021-12-07 19:52:28 +0800

    Add debug sh script

---
+++ debug.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Run test node"
+RUST_LOG=moonbeam_service,moonbeam_rpc_debug,moonbeam_rpc_trace,moonbeam_client_evm_tracing,runtime_common,moonbeam_evm_tracer=debug \
+    ./target/release/moonbeam \
+    --dev --tmp \
+    --sealing 6000 \
+    --execution native \
+    --wasm-execution compiled \
+    --state-cache-size 1 \
+    --ethapi=debug,trace,txpool \
+    # --wasm-runtime-overrides=bear-tracing \
+     > debug.log 2>&1
