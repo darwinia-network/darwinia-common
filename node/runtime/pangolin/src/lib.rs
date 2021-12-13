97d1271337568645a1af7719ad804a88b832fad6
ouwenkg <2630582710@qq.com>
2021-12-13 15:32:08 +0800

    Remove rustc warning

--- node/runtime/pangolin/src/lib.rs
+++ node/runtime/pangolin/src/lib.rs
@@ -686,8 +686,8 @@ sp_api::impl_runtime_apis! {
 
 	impl dp_evm_trace_apis::DebugRuntimeApi<Block> for Runtime {
 		fn trace_transaction(
-			extrinsics: Vec<<Block as BlockT>::Extrinsic>,
-			traced_transaction: &dvm_ethereum::TransactionV0,
+			_extrinsics: Vec<<Block as BlockT>::Extrinsic>,
+			_traced_transaction: &dvm_ethereum::TransactionV0,
 		) -> Result<
 			(),
 			sp_runtime::DispatchError,
@@ -698,10 +698,10 @@ sp_api::impl_runtime_apis! {
 				use dvm_ethereum::Call::transact;
 				// Apply the a subset of extrinsics: all the substrate-specific or ethereum
 				// transactions that preceded the requested transaction.
-				for ext in extrinsics.into_iter() {
+				for ext in _extrinsics.into_iter() {
 					let _ = match &ext.function {
 						Call::Ethereum(transact(transaction)) => {
-							if transaction == traced_transaction {
+							if transaction == _traced_transaction {
 								EvmTracer::new().trace(|| Executive::apply_extrinsic(ext));
 								return Ok(());
 							} else {
@@ -722,8 +722,8 @@ sp_api::impl_runtime_apis! {
 			))
 		}
 		fn trace_block(
-			extrinsics: Vec<<Block as BlockT>::Extrinsic>,
-			known_transactions: Vec<H256>,
+			_extrinsics: Vec<<Block as BlockT>::Extrinsic>,
+			_known_transactions: Vec<H256>,
 		) -> Result<
 			(),
 			sp_runtime::DispatchError,
@@ -738,12 +738,12 @@ sp_api::impl_runtime_apis! {
 				config.estimate = true;
 
 				// Apply all extrinsics. Ethereum extrinsics are traced.
-				for ext in extrinsics.into_iter() {
+				for ext in _extrinsics.into_iter() {
 					match &ext.function {
 						Call::Ethereum(transact(transaction)) => {
 							let eth_extrinsic_hash =
 								H256::from_slice(Keccak256::digest(&rlp::encode(transaction)).as_slice());
-							if known_transactions.contains(&eth_extrinsic_hash) {
+							if _known_transactions.contains(&eth_extrinsic_hash) {
 								// Each known extrinsic is a new call stack.
 								EvmTracer::emit_new();
 								EvmTracer::new().trace(|| Executive::apply_extrinsic(ext));
