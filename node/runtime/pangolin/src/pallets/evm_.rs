46ff332e4a404d5d93ee1191d62c03950367baff
xiaocheng <xiaoch2010@gmail.com>
2021-12-27 14:01:51 +0800

    fix compile

--- node/runtime/pangolin/src/pallets/evm_.rs
+++ node/runtime/pangolin/src/pallets/evm_.rs
@@ -19,6 +19,7 @@ use darwinia_evm_precompile_bridge_ethereum::EthereumBridge;
 use darwinia_evm_precompile_bridge_s2s::Sub2SubBridge;
 use darwinia_evm_precompile_dispatch::Dispatch;
 use darwinia_evm_precompile_simple::{ECRecover, Identity, Ripemd160, Sha256};
+use darwinia_evm_precompile_transfer::Transfer;
 use darwinia_support::{
 	evm::ConcatConverter,
 	s2s::{LatestMessageNoncer, RelayMessageSender},
