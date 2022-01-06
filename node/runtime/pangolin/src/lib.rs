1caa38119d103b6e9578cf30d6394da857950403
bear <boundless.forest@outlook.com>
2022-01-06 10:54:26 +0800

    Fix compile in `evm-tracing` mode

--- node/runtime/pangolin/src/lib.rs
+++ node/runtime/pangolin/src/lib.rs
@@ -753,7 +753,7 @@ sp_api::impl_runtime_apis! {
 				// Apply the a subset of extrinsics: all the substrate-specific or ethereum
 				// transactions that preceded the requested transaction.
 				for ext in _extrinsics.into_iter() {
-					let _ = match &ext.function {
+					let _ = match &ext.0.function {
 						Call::Ethereum(transact { transaction }) => {
 							if transaction == _traced_transaction {
 								EvmTracer::new().trace(|| Executive::apply_extrinsic(ext));
@@ -793,7 +793,7 @@ sp_api::impl_runtime_apis! {
 
 				// Apply all extrinsics. Ethereum extrinsics are traced.
 				for ext in _extrinsics.into_iter() {
-					match &ext.function {
+					match &ext.0.function {
 						Call::Ethereum(transact { transaction }) => {
 							let eth_extrinsic_hash =
 								H256::from_slice(Keccak256::digest(&rlp::encode(transaction)).as_slice());
