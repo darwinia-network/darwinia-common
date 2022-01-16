186237b032bc268d17029c0850c46ace47a880bc
AurevoirXavier <xavier@inv.cafe>
2022-01-16 20:19:31 +0800

    Purge Debug Cache (#1045)

--- .maintain/purge-large-cache.sh
+++ .maintain/purge-large-cache.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+cargo clean -p drml 2> /dev/null || true
+cargo clean -p drml-service 2> /dev/null || true
+cargo clean -p pangoro-runtime 2> /dev/null || true
+cargo clean -p pangolin-runtime 2> /dev/null || true
+cargo clean -p template-runtime 2> /dev/null || true
+rm -rf target/debug/wbuild 2> /dev/null || true
+
 cargo clean --release -p drml 2> /dev/null || true
 cargo clean --release -p drml-service 2> /dev/null || true
 cargo clean --release -p pangoro-runtime 2> /dev/null || true
