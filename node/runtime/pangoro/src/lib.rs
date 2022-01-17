f21c6c60fa809a4ebed9eb7fa7fbb4c3f9ff314a
bear <boundless.forest@outlook.com>
2022-01-17 17:34:52 +0800

    Clean up migration

--- node/runtime/pangoro/src/lib.rs
+++ node/runtime/pangoro/src/lib.rs
@@ -547,13 +547,8 @@ sp_api::impl_runtime_apis! {
 }
 
 fn migrate() -> Weight {
-	frame_support::migrations::migrate_from_pallet_version_to_storage_version::<AllPalletsWithSystem>(
-		&RocksDbWeight::get(),
-	);
-	migration::remove_storage_prefix(b"RandomnessCollectiveFlip", b"RandomMaterial", b"");
-
-	// 0
-	RuntimeBlockWeights::get().max_block
+	0
+	// RuntimeBlockWeights::get().max_block
 }
 
 pub struct CustomOnRuntimeUpgrade;
