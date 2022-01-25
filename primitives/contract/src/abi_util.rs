112795e8567c4cc655986d5ca7b0b54e65f0385a
xiaocheng <xiaoch2010@gmail.com>
2022-01-25 16:43:05 +0800

    use into_iter

--- primitives/contract/src/abi_util.rs
+++ primitives/contract/src/abi_util.rs
@@ -50,16 +50,7 @@ pub fn abi_encode_u64(data: u64) -> Vec<u8> {
 	ethabi::encode(&[Token::Uint(data.into())])
 }
 
-pub fn abi_encode_bytes32(data: [u8; 32]) -> Vec<u8> {
-	ethabi::encode(&[Token::FixedBytes(data.to_vec())])
-}
-
-pub fn abi_encode_array_bytes32(data: Vec<[u8; 32]>) -> Vec<u8> {
-	let array = data.iter().map(|v| Token::FixedBytes(v.to_vec())).collect();
-	ethabi::encode(&[Token::Array(array)])
-}
-
 pub fn abi_encode_array_bytes(data: Vec<Vec<u8>>) -> Vec<u8> {
-	let array = data.iter().map(|v| Token::Bytes(v.to_vec())).collect();
+	let array = data.into_iter().map(|v| Token::Bytes(v)).collect();
 	ethabi::encode(&[Token::Array(array)])
 }
