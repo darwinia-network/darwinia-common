cbf3713ea7e275c6decda55387c67587b3bf53c9
bear <boundless.forest@outlook.com>
2022-03-30 15:33:10 +0800

    Format

--- primitives/evm-trace/ext/src/lib.rs
+++ primitives/evm-trace/ext/src/lib.rs
@@ -1,19 +1,20 @@
-// This file is part of Frontier.
+// This file is part of Darwinia.
 //
-// Copyright (C) 2017-2020 Parity Technologies (UK) Ltd.
-// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2018-2022 Darwinia Network
+// SPDX-License-Identifier: GPL-3.0
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// Darwinia is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
-// 	http://www.apache.org/licenses/LICENSE-2.0
+// Darwinia is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// You should have received a copy of the GNU General Public License
+// along with Darwinia. If not, see <https://www.gnu.org/licenses/>.
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
@@ -21,12 +22,11 @@
 #[cfg(feature = "std")]
 use codec::Decode;
 // --- paritytech ---
-use sp_runtime_interface::runtime_interface;
-use sp_std::vec::Vec;
-//  --- darwinia-network ---
 use evm_tracing_events::StepEventFilter;
 #[cfg(feature = "std")]
 use evm_tracing_events::{Event, EvmEvent, GasometerEvent, RuntimeEvent};
+use sp_runtime_interface::runtime_interface;
+use sp_std::vec::Vec;
 
 #[runtime_interface]
 pub trait DvmExt {
--- primitives/evm-trace/tracer/src/lib.rs
+++ primitives/evm-trace/tracer/src/lib.rs
@@ -1,26 +1,20 @@
-// Copyright 2019-2022 PureStake Inc.
-// This file is part of Moonbeam.
-
-// Moonbeam is free software: you can redistribute it and/or modify
+// This file is part of Darwinia.
+//
+// Copyright (C) 2018-2022 Darwinia Network
+// SPDX-License-Identifier: GPL-3.0
+//
+// Darwinia is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-
-// Moonbeam is distributed in the hope that it will be useful,
+//
+// Darwinia is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
-
+//
 // You should have received a copy of the GNU General Public License
-// along with Moonbeam.  If not, see <http://www.gnu.org/licenses/>.
-
-//! Substrate EVM tracing.
-//!
-//! The purpose of this crate is enable tracing the EVM opcode execution and will be used by
-//! both Dapp developers - to get a granular view on their transactions - and indexers to access
-//! the EVM callstack (internal transactions).
-//!
-//! Proxies EVM messages to the host functions.
+// along with Darwinia. If not, see <https://www.gnu.org/licenses/>.
 
 #![cfg_attr(not(feature = "std"), no_std)]
 pub mod tracer {
@@ -29,9 +23,8 @@ pub mod tracer {
 	use evm::tracing::{using as evm_using, EventListener as EvmListener};
 	use evm_gasometer::tracing::{using as gasometer_using, EventListener as GasometerListener};
 	use evm_runtime::tracing::{using as runtime_using, EventListener as RuntimeListener};
-	// --- darwinia-network ---
-	use evm_tracing_events::{EvmEvent, GasometerEvent, RuntimeEvent, StepEventFilter};
 	// --- paritytech ---
+	use evm_tracing_events::{EvmEvent, GasometerEvent, RuntimeEvent, StepEventFilter};
 	use sp_std::{cell::RefCell, rc::Rc};
 
 	struct ListenerProxy<T>(pub Rc<RefCell<T>>);
