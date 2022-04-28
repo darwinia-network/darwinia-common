f756b6901ab30b2b09d055f0d68445c6d37dc466
bear <boundless.forest@outlook.com>
2022-04-28 14:28:13 +0800

    Add others

--- frame/dvm/evm/precompiles/fee-market/src/lib.rs
+++ frame/dvm/evm/precompiles/fee-market/src/lib.rs
@@ -21,28 +21,58 @@
 // --- core ---
 use core::marker::PhantomData;
 // --- crates.io ---
-// use evm::ExitRevert;
+use evm::ExitRevert;
 // --- darwinia-network ---
 use darwinia_evm_precompile_utils::{PrecompileHelper, StateMutability};
+use dp_contract::abi_util::abi_encode_u64;
+
 // --- paritytech ---
 use fp_evm::{
 	Context, ExitSucceed, Precompile, PrecompileFailure, PrecompileOutput, PrecompileResult,
 };
+use sp_runtime::SaturatedConversion;
+
+#[darwinia_evm_precompile_utils::selector]
+enum Action {
+	MarketFee = "market_fee()",
+}
 
 pub struct FeeMarket<T> {
 	_marker: PhantomData<T>,
 }
 
-impl<T> Precompile for FeeMarket<T> {
-    fn execute(
+impl<T> Precompile for FeeMarket<T>
+where
+	T: darwinia_evm::Config + darwinia_fee_market::Config,
+{
+	fn execute(
 		input: &[u8],
 		target_gas: Option<u64>,
 		context: &Context,
 		is_static: bool,
 	) -> PrecompileResult {
-        todo!()
-    }
+		let mut helper = PrecompileHelper::<T>::new(input, target_gas);
+		let (selector, _data) = helper.split_input()?;
+		let action = Action::from_u32(selector)?;
+
+		// Check state modifiers
+		helper.check_state_modifier(context, is_static, StateMutability::View)?;
+
+		let output = match action {
+			Action::MarketFee => {
+				// Storage: FeeMarket AssignedRelayers (r:1 w:0)
+				helper.record_gas(1, 0)?;
+
+				darwinia_fee_market::Pallet::<T>::market_fee()
+					.map_or(0, |f| f.saturated_into::<u64>())
+			},
+		};
+
+		Ok(PrecompileOutput {
+			exit_status: ExitSucceed::Returned,
+			cost: helper.used_gas(),
+			output: abi_encode_u64(output),
+			logs: Default::default(),
+		})
+	}
 }
-
-
-
