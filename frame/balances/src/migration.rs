8f153319681a18506313e20742bb43d359294bf9
Xavier Lau <xavier@inv.cafe>
2022-05-05 01:06:53 +0800

    Add Migration
    Signed-off-by: Xavier Lau <xavier@inv.cafe>

--- frame/balances/src/migration.rs
+++ frame/balances/src/migration.rs
@@ -7,4 +7,39 @@ pub mod try_runtime {
 #[cfg(feature = "try-runtime")]
 pub use try_runtime::*;
 
-pub fn migrate() {}
+// --- darwinia-network ---
+use crate::*;
+
+pub fn migrate<T, I>()
+where
+	T: Config<I>,
+	I: 'static,
+{
+	// --- darwinia-network ---
+	use darwinia_support::balance::*;
+	// --- paritytech ---
+	use frame_support::WeakBoundedVec;
+	use sp_std::prelude::*;
+
+	<Locks<T, I>>::translate::<
+		WeakBoundedVec<OldBalanceLock<T::Balance, T::BlockNumber>, T::MaxLocks>,
+		_,
+	>(|_, locks| {
+		Some(WeakBoundedVec::force_from(
+			locks
+				.into_inner()
+				.into_iter()
+				.map(|OldBalanceLock { id, lock_for, reasons }| BalanceLock::<T::Balance> {
+					id,
+					amount: match lock_for {
+						LockFor::Common { amount } => amount,
+						LockFor::Staking(staking_lock) =>
+							staking_lock.staking_amount + staking_lock.total_unbond(),
+					},
+					reasons,
+				})
+				.collect::<Vec<_>>(),
+			None,
+		))
+	});
+}
