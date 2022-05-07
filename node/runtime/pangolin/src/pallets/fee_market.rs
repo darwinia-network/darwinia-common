4f94a1b060d52ffc0f8795ad1db8b7f688bc091f
bear <boundless.forest@outlook.com>
2022-05-07 11:57:31 +0800

    Shorten Feemarket Slot (#1209)

--- node/runtime/pangolin/src/pallets/fee_market.rs
+++ node/runtime/pangolin/src/pallets/fee_market.rs
@@ -25,10 +25,9 @@ impl<T: Config<I>, I: 'static> Slasher<T, I> for FeeMarketSlasher {
 
 frame_support::parameter_types! {
 	// Shared configurations.
-	pub const TreasuryPalletId: PalletId = PalletId(*b"da/trsry");
 	pub const MinimumRelayFee: Balance = 15 * COIN;
 	pub const CollateralPerOrder: Balance = 50 * COIN;
-	pub const Slot: BlockNumber = 600;
+	pub const Slot: BlockNumber = 300;
 	pub const AssignedRelayersRewardRatio: Permill = Permill::from_percent(60);
 	pub const MessageRelayersRewardRatio: Permill = Permill::from_percent(80);
 	pub const ConfirmRelayersRewardRatio: Permill = Permill::from_percent(20);
