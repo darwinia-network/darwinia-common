fae7041f2aab341093b3b9c4ead34aace2fab3ee
bear <boundless.forest@outlook.com>
2022-05-07 11:20:10 +0800

    Shorten Slot

--- node/runtime/pangoro/src/pallets/fee_market.rs
+++ node/runtime/pangoro/src/pallets/fee_market.rs
@@ -27,12 +27,12 @@ where
 
 frame_support::parameter_types! {
 	pub const FeeMarketPalletId: PalletId = PalletId(*b"da/feemk");
-	pub const TreasuryPalletId: PalletId = PalletId(*b"da/trsry");
+	// pub const TreasuryPalletId: PalletId = PalletId(*b"da/trsry");
 	pub const FeeMarketLockId: LockIdentifier = *b"da/feelf";
 
 	pub const MinimumRelayFee: Balance = 15 * COIN;
-	pub const CollateralPerOrder: Balance = 60 * COIN;
-	pub const Slot: BlockNumber = 600;
+	pub const CollateralPerOrder: Balance = 50 * COIN;
+	pub const Slot: BlockNumber = 300;
 
 	pub const AssignedRelayersRewardRatio: Permill = Permill::from_percent(60);
 	pub const MessageRelayersRewardRatio: Permill = Permill::from_percent(80);
