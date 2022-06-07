04a90ca6fc7496691f80356a6a7f43210c82e3ab
bear <boundless.forest@outlook.com>
2022-06-07 16:36:48 +0800

    Compile

--- frame/dvm/ethereum/src/lib.rs
+++ frame/dvm/ethereum/src/lib.rs
@@ -486,7 +486,6 @@ impl<T: Config> Pallet<T> {
 			return Err(InvalidTransaction::Payment.into());
 		};
 
-		println!("bear: --- gas_price {:?} base_fee {:?}", gas_price, base_fee);
 		if gas_price < base_fee {
 			return Err(InvalidTransaction::Payment.into());
 		}
@@ -503,7 +502,6 @@ impl<T: Config> Pallet<T> {
 
 		let account_data = <T as darwinia_evm::Config>::RingAccountBasic::account_basic(&origin);
 		let total_payment = transaction_data.value.saturating_add(fee);
-		println!("bear: --- account_data {:?} total_payment {:?}", account_data, total_payment);
 		if account_data.balance < total_payment {
 			return Err(InvalidTransaction::Payment.into());
 		}
--- node/runtime/pangoro/src/pallets/bridge_dispatch.rs
+++ node/runtime/pangoro/src/pallets/bridge_dispatch.rs
@@ -1,11 +1,43 @@
 pub use pallet_bridge_dispatch::Instance1 as WithPangolinDispatch;
 
 // --- paritytech ---
-use frame_support::traits::Everything;
+use frame_support::{traits::Everything, weights::PostDispatchInfo};
+use sp_runtime::transaction_validity::TransactionValidityError;
 // --- darwinia-network ---
 use crate::*;
 use bp_messages::{LaneId, MessageNonce};
-use pallet_bridge_dispatch::Config;
+use darwinia_support::evm::DeriveEthereumAddress;
+use pallet_bridge_dispatch::{Config, EthereumCallDispatch};
+
+pub struct EthereumCallDispatcher;
+impl EthereumCallDispatch for EthereumCallDispatcher {
+	type AccountId = bp_pangolin::AccountId;
+	type Call = Call;
+
+	fn dispatch(
+		c: &Call,
+		origin: &bp_pangolin::AccountId,
+	) -> Result<
+		Option<sp_runtime::DispatchResultWithInfo<PostDispatchInfo>>,
+		TransactionValidityError,
+	> {
+		match c {
+			call @ Call::Ethereum(darwinia_ethereum::Call::transact { transaction: tx }) => {
+				let derive_eth_address = origin.derive_ethereum_address();
+				if let Err(validate_err) =
+					Ethereum::validate_transaction_in_block(derive_eth_address, tx)
+				{
+					return Err(validate_err);
+				}
+
+				Ok(Some(call.clone().dispatch(Origin::from(
+					darwinia_ethereum::RawOrigin::EthereumTransaction(derive_eth_address),
+				))))
+			},
+			_ => Ok(None),
+		}
+	}
+}
 
 impl Config<WithPangolinDispatch> for Runtime {
 	type AccountIdConverter = bp_pangoro::AccountIdConverter;
@@ -13,6 +45,7 @@ impl Config<WithPangolinDispatch> for Runtime {
 	type Call = Call;
 	type CallFilter = Everything;
 	type EncodedCall = bm_pangolin::FromPangolinEncodedCall;
+	type EthereumCallDispatcher = EthereumCallDispatcher;
 	type Event = Event;
 	type SourceChainAccountId = bp_pangolin::AccountId;
 	type TargetChainAccountPublic = bp_pangoro::AccountPublic;
