f7389838c30ff6ebf6294d65c4bbc4d97ad1383e
HackFisher <hackfisher@gmail.com>
2022-06-17 23:31:30 +0800

    Add message transact dispatch all, only used by message layer (#1296)
    * add message transact dispatch all, only used by message layer
    
    * disable transact functionality if PreLog exist

--- frame/dvm/ethereum/src/lib.rs
+++ frame/dvm/ethereum/src/lib.rs
@@ -277,6 +277,39 @@ pub mod pallet {
 			Self::apply_validated_transaction(source, transaction)
 		}
 
+		/// This is message transact only for substrate to substrate LCMP to call
+		#[pallet::weight(<T as darwinia_evm::Config>::GasWeightMapping::gas_to_weight(
+		Pallet::<T>::transaction_data(transaction).gas_limit.unique_saturated_into()
+		))]
+		pub fn message_transact(
+			origin: OriginFor<T>,
+			transaction: Transaction,
+		) -> DispatchResultWithPostInfo {
+			// Source address supposed to be derived address generate from message layer
+			let source = ensure_ethereum_transaction(origin)?;
+
+			// Disable transact functionality if PreLog exist.
+			ensure!(
+				fp_consensus::find_pre_log(&frame_system::Pallet::<T>::digest()).is_err(),
+				Error::<T>::PreLogExists,
+			);
+
+			let extracted_transaction = match transaction {
+				Transaction::Legacy(t) => Ok(Transaction::Legacy(ethereum::LegacyTransaction {
+					nonce: <T as darwinia_evm::Config>::RingAccountBasic::account_basic(&source).nonce,	// auto set
+					gas_price: T::FeeCalculator::min_gas_price(),	// auto set
+					gas_limit: t.gas_limit,
+					action: t.action,
+					value: t.value,
+					input: t.input,
+					signature: t.signature,	// not used.
+				})) ,
+				_ => Err(Error::<T>::MessageTransactionError),
+			}?;
+
+			Self::apply_validated_transaction(source, extracted_transaction)
+		}
+
 		/// Internal transaction only for root.
 		#[pallet::weight(10_000_000)]
 		pub fn root_transact(
@@ -320,6 +353,8 @@ pub mod pallet {
 		InternalTransactionFatalError,
 		/// The internal call failed.
 		ReadyOnlyCall,
+		/// Message transaction invalid
+		MessageTransactionError
 	}
 
 	/// Current building block's transactions and receipts.
