61b3e06030eaadbb55db84885005a7713d841f1d
Xavier Lau <xavier@inv.cafe>
2022-07-05 00:06:35 +0800

    Fix Compile

--- node/service/src/service/mod.rs
+++ node/service/src/service/mod.rs
@@ -320,10 +320,6 @@ where
 			create_inherent_data_providers: move |parent, ()| {
 				let client_clone = client_clone.clone();
 				async move {
-					let uncles = sc_consensus_uncles::create_uncles_inherent_data_provider(
-						&*client_clone,
-						parent,
-					)?;
 					let timestamp = sp_timestamp::InherentDataProvider::from_system_time();
 					let slot =
 						sp_consensus_babe::inherents::InherentDataProvider::from_timestamp_and_duration(
@@ -331,7 +327,7 @@ where
 							slot_duration,
 						);
 
-					Ok((timestamp, slot, uncles))
+					Ok((timestamp, slot))
 				}
 			},
 			force_authoring,
@@ -540,7 +536,7 @@ where
 					slot_duration,
 				);
 
-			Ok((timestamp, slot, uncles))
+			Ok((timestamp, slot))
 		},
 		&task_manager.spawn_essential_handle(),
 		config.prometheus_registry(),
