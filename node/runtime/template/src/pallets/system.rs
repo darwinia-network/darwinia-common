e7253469e1b142a94419f02cadaaf7e40462af58
Xavier Lau <xavier@inv.cafe>
2022-07-19 15:22:21 +0800

    Eliminate Warning

--- node/runtime/template/src/pallets/system.rs
+++ node/runtime/template/src/pallets/system.rs
@@ -1,7 +1,6 @@
 // --- paritytech ---
 use frame_support::{traits::Everything, weights::constants::RocksDbWeight};
 use frame_system::Config;
-use sp_runtime::traits::AccountIdLookup;
 use sp_version::RuntimeVersion;
 // --- darwinia-network ---
 use crate::*;
