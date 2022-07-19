51c15548403eeb087f0acb57d49fe5c452b373c6
Xavier Lau <xavier@inv.cafe>
2022-07-19 15:57:53 +0800

    Update `used_addresses`

--- node/runtime/pangoro/src/pallets/evm.rs
+++ node/runtime/pangoro/src/pallets/evm.rs
@@ -56,11 +56,22 @@ where
 		Self(Default::default())
 	}
 
-	pub fn used_addresses() -> sp_std::vec::Vec<H160> {
-		sp_std::vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 21, 1024, 1025, 2048, 2049]
-			.into_iter()
-			.map(|x| addr(x))
-			.collect()
+	pub fn used_addresses() -> [H160; 13] {
+		[
+			addr(1),
+			addr(2),
+			addr(3),
+			addr(4),
+			addr(5),
+			addr(6),
+			addr(7),
+			addr(8),
+			addr(9),
+			addr(21),
+			addr(1024),
+			addr(1025),
+			addr(2048),
+		]
 	}
 }
 
