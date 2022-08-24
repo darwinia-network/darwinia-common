455da1f6bfcaf5b5bf6df5efda5fb7e19d7c7f04
Xavier Lau <xavier@inv.cafe>
2022-08-24 23:43:49 +0800

    Remove Old Migration (#1414)

--- frame/bridge/ecdsa-authority/src/lib.rs
+++ frame/bridge/ecdsa-authority/src/lib.rs
@@ -187,22 +187,6 @@ pub mod pallet {
 	pub struct Pallet<T>(PhantomData<T>);
 	#[pallet::hooks]
 	impl<T: Config> Hooks<T::BlockNumber> for Pallet<T> {
-		fn on_runtime_upgrade() -> Weight {
-			if let Ok(v) = BoundedVec::try_from(vec![sp_core::H160([
-				104, 137, 141, 177, 1, 40, 8, 128, 140, 144, 63, 57, 9, 9, 197, 45, 159, 112, 103,
-				73,
-			])]) {
-				<Authorities<T>>::put(v.clone());
-				<NextAuthorities<T>>::put(v);
-			}
-			<Nonce<T>>::kill();
-			<AuthoritiesChangeToSign<T>>::kill();
-			<NewMessageRootToSign<T>>::kill();
-			<PreviousMessageRoot<T>>::kill();
-
-			T::DbWeight::get().reads_writes(0, 6)
-		}
-
 		fn on_initialize(now: T::BlockNumber) -> Weight {
 			if (now % T::SyncInterval::get()).is_zero() {
 				if let Some(message_root) = Self::try_update_message_root(now) {
