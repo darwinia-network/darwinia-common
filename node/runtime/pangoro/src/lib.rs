8e987f986672f6f010fafe3ee57a5d79d638885a
Xavier Lau <xavier@inv.cafe>
2022-09-07 10:46:04 +0800

    Bump Runtime Version (#1427)

--- node/runtime/pangoro/src/lib.rs
+++ node/runtime/pangoro/src/lib.rs
@@ -95,7 +95,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: sp_runtime::create_runtime_str!("Pangoro"),
 	impl_name: sp_runtime::create_runtime_str!("Pangoro"),
 	authoring_version: 0,
-	spec_version: 2_9_04_0,
+	spec_version: 2_9_04_1,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 0,
