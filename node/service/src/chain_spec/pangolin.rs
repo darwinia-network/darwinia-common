9b00aefe7208c8bd97d292d192c0df783412baf3
Xavier Lau <xavier@inv.cafe>
2022-09-07 10:29:02 +0800

    Release `v2.9.4` (#1426)
    * Bump Runtime Version
    
    * Bump Crate Version
    
    * Use DNS
    
    * Rebuild Genesis
    
    Co-authored-by: HackFisher <hackfisher@gmail.com>

--- node/service/src/chain_spec/pangolin.rs
+++ node/service/src/chain_spec/pangolin.rs
@@ -304,10 +304,10 @@ pub fn genesis_config() -> ChainSpec {
 		ChainType::Live,
 		genesis,
 		[
-			"/dns4/g1.pangolin-p2p.darwinia.network/tcp/30333/p2p/12D3KooWLc6ZD4PGjnRz8CuVioW1dEr8rVBVEAFb1vpxFHXU4g2Y",
-			"/dns4/g2.pangolin-p2p.darwinia.network/tcp/30333/p2p/12D3KooWHf1v45q3u1qPrkwSUq7ybzNfXf5ELPcpoBTJ4k49axfk",
-			"/dns4/g3.pangolin-p2p.darwinia.network/tcp/30333/p2p/12D3KooWCXW7Ds6invyE1rF4BSfwpMgNKzzBxbnEGGjcqZ6cSgap",
-			"/dns4/g4.pangolin-p2p.darwinia.network/tcp/30333/p2p/12D3KooWHokmaoAJp2vVPkw2YG3HFa799RUAJvdfy4dcaEzBdkGw",
+			"/dns/g1.pangolin-p2p.darwinia.network/tcp/30333/p2p/12D3KooWLc6ZD4PGjnRz8CuVioW1dEr8rVBVEAFb1vpxFHXU4g2Y",
+			"/dns/g2.pangolin-p2p.darwinia.network/tcp/30333/p2p/12D3KooWHf1v45q3u1qPrkwSUq7ybzNfXf5ELPcpoBTJ4k49axfk",
+			"/dns/g3.pangolin-p2p.darwinia.network/tcp/30333/p2p/12D3KooWCXW7Ds6invyE1rF4BSfwpMgNKzzBxbnEGGjcqZ6cSgap",
+			"/dns/g4.pangolin-p2p.darwinia.network/tcp/30333/p2p/12D3KooWHokmaoAJp2vVPkw2YG3HFa799RUAJvdfy4dcaEzBdkGw",
 		]
 		.iter()
 		.filter_map(|s| FromStr::from_str(s).ok())
