ff09692f9642ea3d22a4191465f64f2a284792ef
bear <boundless.forest@outlook.com>
2022-09-09 14:11:24 +0800

    Delete dp-s2s primitive

--- node/runtime/pangoro/src/bridges_message/pangolin.rs
+++ node/runtime/pangoro/src/bridges_message/pangolin.rs
@@ -30,9 +30,8 @@ use sp_runtime::{traits::Zero, FixedPointNumber, FixedU128};
 use sp_std::{ops::RangeInclusive, prelude::*};
 // --- darwinia-network ---
 use crate::*;
-use bp_message_dispatch::CallOrigin;
 use bp_messages::{source_chain::*, target_chain::*, *};
-use bp_runtime::{messages::*, ChainId, *};
+use bp_runtime::{ChainId, *};
 use bridge_runtime_common::{
 	lanes::*,
 	messages::{
@@ -42,7 +41,6 @@ use bridge_runtime_common::{
 		BalanceOf, *,
 	},
 };
-use dp_s2s::{CallParams, CreatePayload};
 use drml_common_runtime::impls::FromThisChainMessageVerifier;
 use pallet_bridge_messages::EXPECTED_DEFAULT_MESSAGE_LENGTH;
 
@@ -77,25 +75,6 @@ pub const INITIAL_PANGOLIN_TO_PANGORO_CONVERSION_RATE: FixedU128 =
 frame_support::parameter_types! {
 	/// Pangolin to Pangoro conversion rate. Initially we treat both tokens as equal.
 	pub storage PangolinToPangoroConversionRate: FixedU128 = INITIAL_PANGOLIN_TO_PANGORO_CONVERSION_RATE;
-}
-
-#[derive(Clone, PartialEq, Eq, Encode, Decode, RuntimeDebug, TypeInfo)]
-pub struct ToPangolinOutboundPayload;
-impl CreatePayload<bp_pangoro::AccountId, bp_pangoro::AccountPublic, bp_pangoro::Signature>
-	for ToPangolinOutboundPayload
-{
-	type Payload = ToPangolinMessagePayload;
-
-	fn create(
-		origin: CallOrigin<bp_pangoro::AccountId, bp_pangoro::AccountPublic, bp_pangoro::Signature>,
-		spec_version: u32,
-		weight: u64,
-		call_params: CallParams,
-		dispatch_fee_payment: DispatchFeePayment,
-	) -> Result<Self::Payload, &'static str> {
-		let call = Self::encode_call(PANGOLIN_S2S_ISSUING_PALLET_INDEX, call_params)?;
-		Ok(ToPangolinMessagePayload { spec_version, weight, origin, call, dispatch_fee_payment })
-	}
 }
 
 /// Pangoro -> Pangolin message lane pallet parameters.
