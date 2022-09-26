94a71eb2343a1db16b6b5b8a70fa80b4009c5756
bear <boundless.forest@outlook.com>
2022-09-26 10:17:23 +0800

    Update `CallValidator` for parachain (#1451)
    * Fix
    
    * Fix CI

--- node/runtime/pangolin/src/pallets/bridge_dispatch.rs
+++ node/runtime/pangolin/src/pallets/bridge_dispatch.rs
@@ -10,7 +10,7 @@ use frame_support::{
 use sp_runtime::transaction_validity::{InvalidTransaction, TransactionValidityError};
 // --- darwinia-network ---
 use crate::*;
-use bp_message_dispatch::{CallValidate, Everything, IntoDispatchOrigin as IntoDispatchOriginT};
+use bp_message_dispatch::{CallValidate, IntoDispatchOrigin as IntoDispatchOriginT};
 use bp_messages::{LaneId, MessageNonce};
 use darwinia_ethereum::{RawOrigin, Transaction};
 use darwinia_evm::CurrencyAdapt;
@@ -117,7 +117,7 @@ impl Config<WithPangolinParachainDispatch> for Runtime {
 	type AccountIdConverter = bp_pangolin::AccountIdConverter;
 	type BridgeMessageId = (LaneId, MessageNonce);
 	type Call = Call;
-	type CallValidator = Everything;
+	type CallValidator = CallValidator;
 	type EncodedCall = bm_pangolin_parachain::FromPangolinParachainEncodedCall;
 	type Event = Event;
 	type IntoDispatchOrigin = IntoDispatchOrigin;
