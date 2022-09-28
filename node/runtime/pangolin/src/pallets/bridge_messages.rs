8c0c886ba7a738a85e047c0e9e9abe7d59068e07
Guantong <04637@163.com>
2022-09-28 11:36:33 +0800

    resolve conflicts

--- node/runtime/pangolin/src/pallets/bridge_messages.rs
+++ node/runtime/pangolin/src/pallets/bridge_messages.rs
@@ -9,7 +9,6 @@ use bp_messages::MessageNonce;
 use bp_runtime::{
 	ChainId, PANGOLIN_PARACHAIN_ALPHA_CHAIN_ID, PANGOLIN_PARACHAIN_CHAIN_ID, PANGORO_CHAIN_ID,
 };
-use darwinia_support::evm::{ConcatConverter, DeriveSubstrateAddress};
 use pallet_bridge_messages::Config;
 use pallet_fee_market::s2s::{
 	FeeMarketMessageAcceptedHandler, FeeMarketMessageConfirmedHandler, FeeMarketPayment,
