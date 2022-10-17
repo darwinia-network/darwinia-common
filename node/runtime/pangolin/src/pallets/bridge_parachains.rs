27017f709df3ec795481f507f396bb55b5cd4865
Xavier Lau <xavier@inv.cafe>
2022-10-17 13:52:09 +0800

    Unify name spec (#1463)

--- node/runtime/pangolin/src/pallets/bridge_parachains.rs
+++ node/runtime/pangolin/src/pallets/bridge_parachains.rs
@@ -1,16 +1,16 @@
 pub use pallet_bridge_parachains::{
-	Instance1 as WithRococoParachainsInstance, Instance2 as WithMoonbaseRelayParachainsInstance,
+	Instance1 as WithRococoParachainInstance, Instance2 as WithMoonbaseRelayParachainInstance,
 };
 
 // --- darwinia-network ---
 use crate::*;
 use pallet_bridge_parachains::Config;
 
-impl Config<WithRococoParachainsInstance> for Runtime {
+impl Config<WithRococoParachainInstance> for Runtime {
 	type BridgesGrandpaPalletInstance = WithRococoGrandpa;
 	type HeadsToKeep = RococoHeadersToKeep;
 }
-impl Config<WithMoonbaseRelayParachainsInstance> for Runtime {
+impl Config<WithMoonbaseRelayParachainInstance> for Runtime {
 	type BridgesGrandpaPalletInstance = WithMoonbaseRelayGrandpa;
 	type HeadsToKeep = RococoHeadersToKeep;
 }
