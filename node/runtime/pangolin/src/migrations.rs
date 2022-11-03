c81137e4cf0d30245e0c413a34af5fbba875c25b
bear <boundless.forest@outlook.com>
2022-11-03 16:12:12 +0800

    Add parachains migration

--- node/runtime/pangolin/src/migrations.rs
+++ node/runtime/pangolin/src/migrations.rs
@@ -23,6 +23,20 @@ impl OnRuntimeUpgrade for CustomOnRuntimeUpgrade {
 }
 
 fn migrate() -> Weight {
+	// paritytech
+	use codec::Decode;
+	use frame_support::{storage::migration::storage_key_iter, Blake2_128Concat, StorageHasher};
+	// darwinia-network
+	use bp_parachains::{BestParaHeadHash, ParaInfo};
+	use bp_polkadot_core::parachains::{ParaHash, ParaId};
+	use bp_runtime::BasicOperatingMode;
+	use bridge_runtime_common::{
+		migrate_message_pallet_operation_mode, migrate_pallet_operation_mode,
+		put_pallet_operation_mode,
+	};
+	use pallet_bridge_parachains::RelayBlockNumber;
+	use sp_api::HeaderT;
+
 	migration::move_pallet(b"BridgeRococoParachains", b"BridgeRococoParachain");
 	migration::move_pallet(b"BridgeMoonbaseRelayParachains", b"BridgeMoonbaseRelayParachain");
 
@@ -39,23 +53,70 @@ fn migrate() -> Weight {
 		items.iter().for_each(|item| migration::remove_storage_prefix(module, item, hash));
 	});
 
-	use bridge_runtime_common::{
-		migrate_message_pallet_operation_mode, migrate_pallet_operation_mode,
-	};
-	let grandpa_modules =
+	// Grandpa pallets
+	let grandpa_modules: Vec<&[u8]> =
 		vec![b"BridgePangoroGrandpa", b"BridgeRococoGrandpa", b"BridgeMoonbaseRelayGrandpa"];
 	for module in grandpa_modules {
+		let item = b"BestFinalized";
+		let hash = &[];
+		if let Some(block_hash) = migration::take_storage_value::<Hash>(module, item, hash) {
+			let imported_header_item = b"ImportedHeaders";
+			let imported_header_hash = frame_support::Identity::hash(&block_hash.encode());
+			if let Some(header) = migration::get_storage_value::<Header>(
+				module,
+				imported_header_item,
+				&imported_header_hash,
+			) {
+				migration::put_storage_value(module, item, hash, (header.number(), header.hash()));
+			}
+		}
+
 		migrate_pallet_operation_mode(module);
-		// TODO, migrate BestFinalized
 	}
 
-	let message_modules = vec![
+	//  Message pallets
+	let message_modules: Vec<&[u8]> = vec![
 		b"BridgePangoroMessages",
 		b"BridgePangolinParachainMessages",
 		b"BridgePangolinParachainAlphaMessages",
 	];
 	for module in message_modules {
 		migrate_message_pallet_operation_mode(module);
+	}
+
+	// Parachains pallets
+	#[derive(Encode, Decode)]
+	pub struct BestParaHead {
+		pub at_relay_block_number: RelayBlockNumber,
+		pub head_hash: ParaHash,
+		pub next_imported_hash_position: u32,
+	}
+	let old_item = b"BestParaHeads";
+	let new_item = b"ParasInfo";
+
+	let parachains_modules: Vec<&[u8]> =
+		vec![b"BridgeRococoParachain", b"BridgeMoonbaseRelayParachain"];
+	for module in parachains_modules {
+		for (para_id, best_para_head) in
+			storage_key_iter::<ParaId, BestParaHead, Blake2_128Concat>(module, old_item).drain()
+		{
+			let para_info = ParaInfo {
+				best_head_hash: BestParaHeadHash {
+					at_relay_block_number: best_para_head.at_relay_block_number,
+					head_hash: best_para_head.head_hash,
+				},
+				next_imported_hash_position: best_para_head.next_imported_hash_position,
+			};
+
+			migration::put_storage_value(
+				module,
+				new_item,
+				&Blake2_128Concat::hash(&para_id.encode()),
+				para_info,
+			);
+		}
+
+		put_pallet_operation_mode(module, BasicOperatingMode::Normal);
 	}
 
 	// 0
