6a9392cfb9fe2c99b1c2b47d0c36125d61991bb7
Xavier Lau <xavier@inv.cafe>
2022-11-04 17:37:45 +0800

    `darwinia-messages-substrate`'s migration companions (#1472)
    Signed-off-by: Xavier Lau <xavier@inv.cafe>
    
    Signed-off-by: Xavier Lau <xavier@inv.cafe>

--- node/runtime/pangoro/src/migrations.rs
+++ node/runtime/pangoro/src/migrations.rs
@@ -25,6 +25,27 @@ impl OnRuntimeUpgrade for CustomOnRuntimeUpgrade {
 fn migrate() -> Weight {
 	migration::remove_storage_prefix(b"TransactionPause", b"PausedTransactions", &[]);
 
+	let grandpa = b"BridgePangolinGrandpa";
+
+	bridge_runtime_common::migrate_pallet_operation_mode(grandpa);
+
+	if let Some(hash) = migration::take_storage_value::<<Pangolin as bp_runtime::Chain>::Hash>(
+		grandpa,
+		b"BestFinalized",
+		&[],
+	) {
+		if let Some(header) =
+			<pallet_bridge_grandpa::ImportedHeaders<Runtime, WithPangolinGrandpa>>::get(hash)
+		{
+			<pallet_bridge_grandpa::BestFinalized<Runtime, WithPangolinGrandpa>>::put((
+				header.number,
+				hash,
+			));
+		}
+	}
+
+	bridge_runtime_common::migrate_message_pallet_operation_mode(b"BridgePangolinMessages");
+
 	// 0
 	RuntimeBlockWeights::get().max_block
 }
