9c4a979044339f1d5ce5241728e6c591c1f0664c
Xavier Lau <xavier@inv.cafe>
2022-11-04 16:15:03 +0800

    `darwinia-messages-substrate`'s migration companions
    Signed-off-by: Xavier Lau <xavier@inv.cafe>

--- node/runtime/pangolin/src/migrations.rs
+++ node/runtime/pangolin/src/migrations.rs
@@ -33,10 +33,55 @@ fn migrate() -> Weight {
 		),
 		(b"TransactionPause", &[b"PausedTransactions"]),
 	];
-	let hash = &[];
 
 	removed_items.iter().for_each(|(module, items)| {
-		items.iter().for_each(|item| migration::remove_storage_prefix(module, item, hash));
+		items.iter().for_each(|item| migration::remove_storage_prefix(module, item, &[]));
+	});
+
+	let grandpas: &[&[u8]] =
+		&[b"BridgePangoroGrandpa", b"BridgeRococoGrandpa", b"BridgeMoonbaseRelayGrandpa"];
+
+	grandpas
+		.iter()
+		.for_each(|grandpa| bridge_runtime_common::migrate_pallet_operation_mode(grandpa));
+
+	macro_rules! migrate_best_finalized {
+		($c:ty, $i:ty, $n:expr) => {
+			if let Some(hash) = migration::take_storage_value::<<$c as bp_runtime::Chain>::Hash>(
+				$n,
+				b"BestFinalized",
+				&[],
+			) {
+				if let Some(header) =
+					<pallet_bridge_grandpa::ImportedHeaders<Runtime, $i>>::get(hash)
+				{
+					<pallet_bridge_grandpa::BestFinalized<Runtime, $i>>::put((header.number, hash));
+				}
+			}
+		};
+	}
+
+	migrate_best_finalized!(Pangoro, WithPangoroGrandpa, b"BridgePangoroGrandpa");
+	migrate_best_finalized!(Rococo, WithRococoGrandpa, b"BridgeRococoGrandpa");
+	migrate_best_finalized!(MoonbaseRelay, WithMoonbaseRelayGrandpa, b"BridgeMoonbaseRelayGrandpa");
+
+	let messages: &[&[u8]] = &[
+		b"BridgePangoroMessages",
+		b"BridgePangolinParachainMessages",
+		b"BridgePangolinParachainAlphaMessages",
+	];
+
+	messages
+		.iter()
+		.for_each(|message| bridge_runtime_common::migrate_message_pallet_operation_mode(message));
+
+	let parachains_modules: &[&[u8]] = &[b"BridgeRococoParachain", b"BridgeMoonbaseRelayParachain"];
+
+	parachains_modules.iter().for_each(|module| {
+		bridge_runtime_common::put_pallet_operation_mode(
+			module,
+			bp_runtime::BasicOperatingMode::Normal,
+		);
 	});
 
 	// 0
