46ef150e0141e7d033ade8fd607e094ef23181da
Xavier Lau <xavier@inv.cafe>
2023-02-16 03:15:52 +0800

    Enable panic

--- node/runtime/pangoro/src/lib.rs
+++ node/runtime/pangoro/src/lib.rs
@@ -97,7 +97,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: sp_runtime::create_runtime_str!("Pangoro"),
 	impl_name: sp_runtime::create_runtime_str!("Pangoro"),
 	authoring_version: 0,
-	spec_version: 210001,
+	spec_version: 3_00_00_0,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 0,
